df6864ca05f69357ede44e7515e208fb783ba362
admin_dho <davidhorph@hotmail.com>
2025-05-17 21:05:17 -0500

    Update Registration.jsx
    Added new class to div.

--- src/components/registration/Registration.jsx
+++ src/components/registration/Registration.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 import './Registration.styles.css';
 
 const Registration = ({ text }) => {
-   
-  return (
-    <div className='registration-div'>
-      <h1>{text}</h1>
-    </div>
-  )
+   const bgColor = text.toLowerCase() == 'login' ? 'login' : 'signup'
+   return (
+      <div className={`registration-div ${bgColor}`}>
+         <h1 style={{textAlign: 'center'}}>{text}</h1>
+      </div>
+   )
 }
 
 export default Registration
