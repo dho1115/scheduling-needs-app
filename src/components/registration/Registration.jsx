00bb940e9c3b930af229fb8cb188f5cc3f4e5868
admin_dho <davidhorph@hotmail.com>
2025-05-17 19:55:47 -0500

    Registration Component.
    Created Registration Component (Registration.jsx).

---
+++ src/components/registration/Registration.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './Registration.styles.css';
+
+const Registration = ({ text }) => {
+   
+  return (
+    <div className='registration-div'>
+      <h1>{text}</h1>
+    </div>
+  )
+}
+
+export default Registration
