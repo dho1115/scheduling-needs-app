f248ade5adf2d9266759cc8a28cb63507a1673a3
admin_dho <davidhorph@hotmail.com>
2025-05-18 07:51:43 -0500

    Created Scheduling Needs Component.
    Where available shifts will be located.

---
+++ src/pages/scheduling_needs/SchedulingNeeds.jsx
@@ -0,0 +1,38 @@
+import { useLocation } from 'react-router-dom';
+import { Container } from 'reactstrap';
+import ErrorBoundary from '../../components/ErrorBoundary';
+
+import './SchedulingNeeds.styles.css';
+
+const SchedulingNeeds = ({shiftsArray}) => {
+   const location = useLocation();
+
+   return (
+      <div className='scheduling-needs-div'>
+         <ErrorBoundary fallback={<h3>Error Occurred inside {location.pathname}.</h3>}>
+            <Container className='available-shifts-container m-1'>
+               <header className='available-shifts-header'>
+                  <h3>CURRENT SHIFTS AVAILABLE</h3>
+               </header>
+               <div className='availableShifts'>
+                  {
+                     shiftsArray.map(({storeNumber, location, date, time, urgent, comments}, idx) => (
+                        <div key={idx}>
+                           {urgent && <h1 style={{color: 'red'}}>URGENT!!!</h1>}
+                           <h1>Store Number: {storeNumber}</h1>
+                           <h3>location: {location}</h3>
+                           <h3>Date: {date}</h3>
+                           <h3>Time: {time}</h3>
+                           <hr />
+                           {comments && <h5>{comments}</h5>}
+                        </div>
+                     ))
+                  }
+               </div>
+            </Container>
+         </ErrorBoundary>
+      </div>
+   )
+}
+
+export default SchedulingNeeds
