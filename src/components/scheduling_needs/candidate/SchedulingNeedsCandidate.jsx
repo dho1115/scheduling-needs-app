b701cd44444e328e8b2bf4669ec6f13f80a6770c
admin_dho <davidhorph@hotmail.com>
2025-05-19 08:02:46 -0500

    New Component Created.
    <SchedulingNeedsCandidate> if the user logs in as a "candidate" (someone LOOKING to work shifts).

---
+++ src/components/scheduling_needs/candidate/SchedulingNeedsCandidate.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Button, Form, FormGroup } from 'reactstrap'
+
+import "./SchedulingNeedsCandidate.styles.css"
+const SchedulingNeedsCandidate = ({_shifID}) => {
+  return (
+    <div>
+      <Form>
+         <FormGroup>
+            <Button color='danger'>SELECT THIS SHIFT!!!</Button>
+         </FormGroup>
+         <FormGroup>
+            <Button color='success'>ONLY DO PARTIAL!!!</Button>
+         </FormGroup>
+      </Form>
+    </div>
+  )
+}
+
+export default SchedulingNeedsCandidate
