4d6482f5854a46686f5d280751abb145c7a4c4df
admin_dho <davidhorph@hotmail.com>
2025-05-20 13:57:13 -0500

    Added createContext
    Added createContext to create a Context.Provider wrapper to hold available shifts and shift applicants.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
+import { createContext, useState } from "react";
 import { BrowserRouter, Route, Routes, } from "react-router-dom";
+
+export const ShiftContext = createContext();
 
 //Pages;
 import Homepage from "./pages/homepage/Homepage";
 
 function App() {
+  const [shifts, setShifts] = useState({});
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-      </Routes>
-    </BrowserRouter>
+    <ShiftContext.Provider value={{shifts, setShifts}}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </BrowserRouter>
+    </ShiftContext.Provider>
   )
 }
 
