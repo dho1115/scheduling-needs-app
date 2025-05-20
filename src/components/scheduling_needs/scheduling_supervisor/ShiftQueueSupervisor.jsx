9d240b8f9bacee31fe6e21f05f3df282531a865f
admin_dho <davidhorph@hotmail.com>
2025-05-20 13:10:25 -0500

    Create ShiftQueueSupervisor.jsx
    New component created.

---
+++ src/components/scheduling_needs/scheduling_supervisor/ShiftQueueSupervisor.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import "./SchedulingNeedsSupervisor.styles.css";
+
+const ShiftQueueSupervisor = () => {
+  return (
+    <div>ShiftQueueSupervisor</div>
+  )
+}
+
+export default ShiftQueueSupervisor
