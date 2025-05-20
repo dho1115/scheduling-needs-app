a0edb5856c309864e2ea08c20377d60eefef8899
admin_dho <davidhorph@hotmail.com>
2025-05-20 13:29:33 -0500

    Update SupervisorPage.jsx
    The components/features I plan to have for the Supervisor page.

--- src/pages/supervisor/SupervisorPage.jsx
+++ src/pages/supervisor/SupervisorPage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import SchedulingNeeds from '../../components/scheduling_needs/SchedulingNeeds';
 
 import "./SupervisorPage.styles.css";
 
 const SupervisorPage = () => {
+  //Scheduling Needs list.
+  //Scheduling Needs applicants.
+  //Scheduling Needs award/rejection.
+  
   return (
     <div>SupervisorPage</div>
   )
