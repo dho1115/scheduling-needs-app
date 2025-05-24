543411fe4723e79dd4fa207b55874f66594fd9a8
admin_dho <davidhorph@hotmail.com>
2025-05-24 17:49:16 -0500

    Update App.jsx
    Added new Routes and changed the values for ShiftContext.Provider.

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,27 @@ import { BrowserRouter, Route, Routes, } from "react-router-dom";
 
 export const ShiftContext = createContext();
 
+//Components;
+import SchedulingNeeds from "./components/scheduling_needs/SchedulingNeeds";
+
 //Pages;
 import Homepage from "./pages/homepage/Homepage";
 import SupervisorPage from "./pages/supervisor/SupervisorPage";
 
 function App() {
-  const [shifts, setShifts] = useState({});
+  const [shiftsArray, setShiftsArray] = useState([]);
+  const [role, setRole] = useState({});
 
   return (
-    <ShiftContext.Provider value={{shifts, setShifts}}>
+    <ShiftContext.Provider
+      value={{ shiftsArray, setShiftsArray, role, setRole }}
+    >
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepage />} />
-          <Route path="/supervisor" element={<SupervisorPage />} />
+          <Route path="/supervisor/*" element={<SupervisorPage />}>
+            <Route path="available shifts" element={<SchedulingNeeds />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </ShiftContext.Provider>
