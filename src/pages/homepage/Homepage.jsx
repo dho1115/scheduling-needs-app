186966033ebb601aa2a72504f6ead9792c2905c2
admin_dho <davidhorph@hotmail.com>
2025-05-26 18:44:59 -0500

    Modified useState for registrationMode
    Changed the state to include register and login keys (before, I used two different useState( ) - one for register and another for login).

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 //Components
 import Registration from '../../components/registration/Registration';
+import Register from '../../components/registration/Forms/Register';
 
 //Dependencies.
 import { Container } from 'reactstrap';
+import Login from '../../components/registration/Forms/Login';
 
 import './Homepage.styles.css';
 
 const Homepage = () => {
+  const [registrationMode, setRegistrationMode] = useState({ register: false, login: false });
+  const registerToggle = () => setRegistrationMode(prvMode => ({...prvMode, register: !prvMode.register, login: false}));
+  const loginToggle = () => setRegistrationMode(prvMode => ({ ...prvMode, register: false, login: !prvMode.login }));
+
+  useEffect(() => {
+    return () => {
+      setRegistrationMode({ register: false, login: false });
+    };
+  }, [])
+
   return (
     <div className='homepage-div'>
-      <Container className='homepage-container'>
-         <Registration text="LOGIN" />
-         <Registration text="SIGN UP" />
+      <Container className='homepage-container' style={{position: (!registrationMode.register && !registrationMode.login) ? 'relative' : 'absolute'}}>        
+        <Registration text="LOGIN" toggle={loginToggle} />
+        <Registration text="SIGN UP" toggle={registerToggle} />
       </Container>
+      {
+        registrationMode.register
+        &&
+        <Register isOpen={registrationMode.register} toggle={registerToggle} />
+      }
+      {
+        registrationMode.login
+        &&
+        <Login isOpen={registrationMode.login} toggle={loginToggle} />
+      }
     </div>
   )
 }
