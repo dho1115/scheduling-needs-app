5bcefefefec491c918d4b745188efc7c664b17b2
admin_dho <davidhorph@hotmail.com>
2025-05-26 18:43:20 -0500

    replaced alert( ).
    Replaced the alert( ) with toggle function that was imported as props.

--- src/components/registration/Registration.jsx
+++ src/components/registration/Registration.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
+import ErrorBoundary from '../ErrorBoundary';
 
 import './Registration.styles.css';
 
-const Registration = ({ text }) => {
-   const bgColor = text.toLowerCase() == 'login' ? 'login' : 'signup'
-   return (
-      <div className={`registration-div ${bgColor}`} onClick={() => alert(`Modal for ${text.toUpperCase()} will appear.`)}>
-         <h1 style={{textAlign: 'center'}}>{text}</h1>
-      </div>
-   )
+const Registration = ({ text, toggle }) => {
+   const bgColor = text.toLowerCase() == 'login' ? 'login' : 'signup';
+   const location = useLocation();
+
+   try {
+      return (
+         <ErrorBoundary fallback={<h1>Oops... something went wrong in {location.pathname}.</h1>}>
+            <div className={`registration-div ${bgColor}`} onClick={toggle}>
+               <h1 style={{textAlign: 'center'}}>{text}</h1>
+            </div>
+         </ErrorBoundary>
+      )
+   } catch (error) {
+      return (
+         <div>
+            <h1>ERROR INSIDE {location.pathname}: {JSON.stringify({error, errorCode: error.code, errorMessage: error.message})}</h1>
+         </div>
+      )
+   }
+
+   
 }
 
 export default Registration
