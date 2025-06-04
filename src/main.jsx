2aebf036a73e0f017287f3a36db601dd6243ab3f
admin_dho <davidhorph@hotmail.com>
2025-06-04 18:03:28 -0500

    Update main.jsx
    Removed strictmode.

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,5 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'; //imported css.
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <App />,
 )
