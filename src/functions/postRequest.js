383924d73544df5696189af7dafb51d1eb046aca
admin_dho <davidhorph@hotmail.com>
2025-06-14 14:19:20 -0500

    New function.
    PostCurrentUser function to log the current user.

--- src/functions/postRequest.js
+++ src/functions/postRequest.js
@@ -5,3 +5,14 @@ export const PostRequest = (url, jsonBody) => fetch(url, { method: 'POST', heade
    })
    .then(data => console.log({ message: `SUCCESS!!! Data is ${JSON.stringify(data.json())}.`, data }))
    .catch(err => console.error({ message: 'POST request error!!!', err, errMessage: err.message, errCode: err.code, data: JSON.stringify(jsonBody) }));
+
+export const PostCurrentUser = (url, jsonBody) => fetch(url, {
+   method: 'POST',
+   headers: { 'Content-Type': 'application/json' },
+   body: JSON.stringify(jsonBody)
+}).then(data => {
+   console.log(`SUCCESS!!! Posted ${JSON.stringify(data)}.`)
+   return data;
+}).then(response => {
+   console.log("RESPONSE:", response.json());
+}).catch(error => console.error({ message: 'PostCurrentUser (function) error!!!', error, status: error.status, code: error.code, errMessage: error.message }));
