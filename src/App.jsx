d30a46115a5594c7f4b71ca35497b78907281251
admin_dho <davidhorph@hotmail.com>
2025-06-15 18:05:47 -0500

    Update App.jsx
    Added employees useState. Also for useFetch, it now accepts a setState function declaration as the second argument.

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,19 @@ export const ShiftContext = createContext();
 import SchedulingNeeds from "./components/scheduling_needs/SchedulingNeeds";
 
 //Pages;
-import
-Homepage from "./pages/homepage/Homepage";
+import Homepage from "./pages/homepage/Homepage";
 import SupervisorPage from "./pages/supervisor/SupervisorPage";
 
 function App() {
   const [shiftsArray, setShiftsArray] = useState([]);
-  const [currentUser, setCurrentUser] = useState({id: '', name: '', password: '', role: ''});
-  const [employees, setEmployees] = useFetch("http://localhost:3003/employees");
+  const [currentUser, setCurrentUser] = useState({ id: '', name: '', password: '', role: '' });
+  const [employees, setEmployees] = useState([]);
 
+  useFetch("http://localhost:3003/employees", data => setEmployees(prvEmployees => ([...prvEmployees, ...data])));
+  useFetch("http://localhost:3003/currentUser", data => setCurrentUser(prvUser => ({ ...prvUser, ...data })));
+  
+  console.log({ currentUser, employees });
+  
   return (
     <ShiftContext.Provider
       value={{ shiftsArray, setShiftsArray, currentUser, setCurrentUser, employees, setEmployees }}
