9413d0f746f8d961981ea9fbaff7b4e0623ab80c
admin_dho <davidhorph@hotmail.com>
2025-06-16 19:59:25 -0500

    Update FetchHook.js
    Added and implemented cleanup function.

--- src/functions/FetchHook.js
+++ src/functions/FetchHook.js
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 
-export function useFetch(url, setStateFn=() => console.log('setState Function goes here.')) {
+export function useFetch(url, setStateFn = () => console.log('setState Function goes here.'), cleanupFunction = null) {
    useEffect(() => {
       fetch(url).then(res => {
+         cleanupFunction && cleanupFunction()
          if (!res.ok) {
             throw new Error(`Error!!! ${res.status}. ERROR MESSAGE: ${res.message}.`)
          }
@@ -10,7 +11,9 @@ export function useFetch(url, setStateFn=() => console.log('setState Function go
       }).then(data => {
          setStateFn(data);
          console.log("From useFetch: SUCCESS!!! ", data);
-      }).catch(err => console.error({err, errCode: err.code, errMessage: err.message}))
+      }).catch(err => console.error({ err, errCode: err.code, errMessage: err.message }))
+      
+      return () => cleanupFunction ? cleanupFunction() : null
    }, []);
 }
 
