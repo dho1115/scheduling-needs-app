de36d3d8c99dfceacb257d565f7a822794e54842
admin_dho <davidhorph@hotmail.com>
2025-06-19 12:06:30 -0500

    Update SupervisorPage.jsx
    Added a fallback.

--- src/pages/supervisor/SupervisorPage.jsx
+++ src/pages/supervisor/SupervisorPage.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { Suspense, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import NavigationBar from '../../components/navigation-bar/NavigationBar';
 import { ShiftContext } from '../../App';
@@ -10,15 +10,17 @@ const SupervisorPage = () => {
   //Scheduling Needs applicants.
   //Scheduling Needs award/rejection.
 
-  const {currentUser, setCurrentUser, shiftsArray, setShiftsArray} = useContext(ShiftContext);
-
+  const { currentUser, setCurrentUser, shiftsArray, setShiftsArray } = useContext(ShiftContext);
+  
   return (
     <div>
-      <NavigationBar />
-      <div className='p-1'>
-        <h3>Welcome to your page, <span style={{color: 'red'}}>{currentUser.name}</span>!!!</h3>
-      </div>
-      <Outlet />
+      <Suspense fallback={<h1>Loading data... please wait while we load {JSON.stringify(currentUser)}.</h1>}>
+        <NavigationBar />
+        <div className='p-1'>
+          <h3>Welcome to your page, <span style={{color: 'red'}}>{currentUser.name}</span>!!!</h3>
+        </div>
+        <Outlet />
+      </Suspense>      
     </div>
   )
 }
