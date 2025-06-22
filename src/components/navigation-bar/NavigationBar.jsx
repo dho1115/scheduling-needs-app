59230b951c1673ccd44e4c78bb6848332f28abf3
admin_dho <davidhorph@hotmail.com>
2025-06-22 14:10:24 -0500

    Update NavigationBar.jsx
    Replaced the static navigation links with the more dynamic NavigationLinks( { id, role } ).filter( ... ).map( ... ).
    
    Tested it out by removing { id, role } arguments. It works.
    
    NOTE: The .filter is used to show only roles that are relevant to THAT logged in user's role. The descriptions for the restrictions can be seen in NavigationLinks.jsx.

--- src/components/navigation-bar/NavigationBar.jsx
+++ src/components/navigation-bar/NavigationBar.jsx
@@ -4,31 +4,43 @@ import { Link, useNavigate } from 'react-router-dom';
 //Dependencies.
 import { PostCurrentUser } from '../../functions/postRequest';
 import { ShiftContext } from '../../App';
+import { NavigationLinks } from './NavigationLinks';
+import ErrorBoundary from '../ErrorBoundary';
 
 import './NavigationBar.styles.css';
 
 const NavigationBar = () => {
-  const { currentUser } = useContext(ShiftContext);
+  const { currentUser, setCurrentUser } = useContext(ShiftContext);
+  const { id, role } = currentUser;  
   const navigate = useNavigate();
   
   const handleLogoff = () => PostCurrentUser(
     'http://localhost:3003/currentUser',
     { id: '', name: '', password: '', role: '', base: '' }
   )
-    .then(result => {
-      console.log(`Successfully logged out!!! currentUser is ${JSON.stringify(currentUser)}. Result is: ${result}.`)
-      navigate("/")
+    .then(() => {
+      setCurrentUser({ id: '', name: '', password: '', role: '' });
     })
+    .then(() => navigate("/"))
     .catch(error => console.error({ message: 'error on handleLogoff function!!!', error, errorCode: error.code, errorStatus: error.status, errorMessage: error.message }));
 
   return (
-      <nav className='navigation p-3'>
-         <strong><Link to="/" className='navlink'>Login Page</Link></strong>
-         <strong><Link to="/supervisor/available shifts" className='navlink'>View Available Shifts</Link></strong>
-         <strong>Add Shift</strong>
-         <strong>Cancel Available Shift</strong>
-         <strong className='logoff' onClick={handleLogoff}>LOG OUT!!!</strong>
-      </nav>
+    <nav className='navigation p-3'>
+      <ErrorBoundary fallback={<h1>COMPILE TIME ERROR IN NavigationBar.jsx!!!</h1>}>
+        {
+          NavigationLinks({ id, role })
+            .filter(({restrictions}) => restrictions == role || !restrictions)
+            .map(({ name, to }, idx) => {
+              return (
+                <Link to={to} key={idx} className='navlink'>
+                  <strong>{name}</strong>
+                </Link>
+              ) //1. Filter out links based on restrictions (see NavigationLinks.jsx for the restrictions). 2. .map() out the results into links.
+            })
+        }
+        <strong className='logoff' onClick={handleLogoff}>LOG OUT!!!</strong>
+      </ErrorBoundary>
+    </nav>
   )
 }
 
