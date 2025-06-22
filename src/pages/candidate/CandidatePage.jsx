a7b80db5e5891a7a6215eac0b0dd74f5cf22d53d
admin_dho <davidhorph@hotmail.com>
2025-06-22 12:58:21 -0500

    <CandidatePage />
    Added Navigation Bar and made height of the page 100vh.

--- src/pages/candidate/CandidatePage.jsx
+++ src/pages/candidate/CandidatePage.jsx
@@ -1,15 +1,22 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
+
+//Dependencies.
 import { ShiftContext } from '../../App';
+
+//Components:
+import NavigationBar from '../../components/navigation-bar/NavigationBar';
 
 import "./CandidatePage.styles.css";
 
 const CandidatePage = () => {
   const { currentUser } = useContext(ShiftContext);
-  const { id, name } = currentUser;
+  const { id, name, role } = currentUser;
 
   return (
     <div className='candidate-page-div'>
+      <NavigationBar />
       <h1>Welcome to the Candidate Homepage, {name}!!!</h1>
+      <h3>ROLE: <span className='text-danger'>{role}</span>.</h3>
     </div>
   )
 }
