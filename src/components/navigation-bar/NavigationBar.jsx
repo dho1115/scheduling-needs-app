63f8058fd50461300f7293450359a2aec9e9114f
admin_dho <davidhorph@hotmail.com>
2025-06-22 12:59:02 -0500

    Update NavigationBar.jsx
    Added a .then( ) that will have a navigate("/") to take user back to the home page.

--- src/components/navigation-bar/NavigationBar.jsx
+++ src/components/navigation-bar/NavigationBar.jsx
@@ -1,16 +1,25 @@
-import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 //Dependencies.
 import { PostCurrentUser } from '../../functions/postRequest';
+import { ShiftContext } from '../../App';
 
 import './NavigationBar.styles.css';
 
-
 const NavigationBar = () => {
+  const { currentUser } = useContext(ShiftContext);
+  const navigate = useNavigate();
+  
   const handleLogoff = () => PostCurrentUser(
     'http://localhost:3003/currentUser',
     { id: '', name: '', password: '', role: '', base: '' }
-  );
+  )
+    .then(result => {
+      console.log(`Successfully logged out!!! currentUser is ${JSON.stringify(currentUser)}. Result is: ${result}.`)
+      navigate("/")
+    })
+    .catch(error => console.error({ message: 'error on handleLogoff function!!!', error, errorCode: error.code, errorStatus: error.status, errorMessage: error.message }));
 
   return (
       <nav className='navigation p-3'>
