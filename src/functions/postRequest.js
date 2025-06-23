fd857981832f6648603880ac3f45eca0d947a3df
admin_dho <davidhorph@hotmail.com>
2025-06-23 15:31:52 -0500

    Removed commented out fn..

--- src/functions/postRequest.js
+++ src/functions/postRequest.js
@@ -1,7 +1,2 @@
 export const PostRequest = (url, jsonBody) => fetch(url, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(jsonBody) })
 
-// export const PostCurrentUser = (url, jsonBody) => fetch(url, {
-//    method: 'POST',
-//    headers: { 'Content-Type': 'application/json' },
-//    body: JSON.stringify(jsonBody)
-// }) //Commented out this duplicate post request.
