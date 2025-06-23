387c8ef6ed439d4863d70a4ffe334f064eddf18d
admin_dho <davidhorph@hotmail.com>
2025-06-23 15:30:23 -0500

    Update postRequest.js
    Commented out the duplicate POST request. PostCurrentUser is the same as PostRequest.
    
    Also, removed the .then( ) and .catch( ). Will append later when I call PostRequest.

--- src/functions/postRequest.js
+++ src/functions/postRequest.js
@@ -1,16 +1,7 @@
 export const PostRequest = (url, jsonBody) => fetch(url, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(jsonBody) })
-   .then(response => {
-      console.log(`Successfully posted ${JSON.stringify(response)}!!!`);
-      return response
-   })
-   .then(data => console.log({ message: `SUCCESS!!! Data is ${JSON.stringify(data.json())}.`, data }))
-   .catch(err => console.error({ message: 'POST request error!!!', err, errMessage: err.message, errCode: err.code, data: JSON.stringify(jsonBody) }));
 
-export const PostCurrentUser = (url, jsonBody) => fetch(url, {
-   method: 'POST',
-   headers: { 'Content-Type': 'application/json' },
-   body: JSON.stringify(jsonBody)
-})
-   .then(data => data)
-   .then(response => console.log("RESPONSE:", response.json()))
-   .catch(error => console.error({ message: 'PostCurrentUser (function) error!!!', error, status: error.status, code: error.code, errMessage: error.message }));
+// export const PostCurrentUser = (url, jsonBody) => fetch(url, {
+//    method: 'POST',
+//    headers: { 'Content-Type': 'application/json' },
+//    body: JSON.stringify(jsonBody)
+// }) //Commented out this duplicate post request.
