30104cf36cccb5bebd7630c5da543cfcbd458611
admin_dho <davidhorph@hotmail.com>
2025-06-27 06:49:13 -0500

    Added ShiftsAppliedFor component.
    This component will show the candidate's shifts that are applied for but are pending approval and will show "no shifts" message if the candidate did not apply for any shifts yet.

---
+++ src/components/private/candidate/pending_shifts/ShiftsAppliedFor.jsx
@@ -0,0 +1,42 @@
+import React, { useContext } from 'react';
+
+//dependencies.
+import { ShiftContext } from '../../../../App'
+import { Container } from 'reactstrap';
+
+import "./ShiftsAppliedFor.styles.css";
+
+const ShiftsAppliedFor = () => {
+  const { currentUser } = useContext(ShiftContext);
+  const shiftsAppliedFor = currentUser.shifts?.applied;
+
+  if (!shiftsAppliedFor) {
+    return (
+      <div>
+        <Container className='no-shifts-container'>
+          <h1>You Have Not Applied for any Shifts (yet)!!!</h1>
+        </Container>
+      </div>
+    )
+  }
+  return (
+    <div className='shiftsappliedfordiv p-1'>
+      <header>
+        <h1>SHIFTS I APPLIED FOR.</h1>
+      </header>
+      <Container>
+         {
+          shiftsAppliedFor.map((val, idx) => {
+            return (
+              <div key={idx}>
+                <strong>{JSON.stringify(val)}</strong>
+              </div>
+            )
+          })
+         }
+      </Container>
+    </div>
+  )
+}
+
+export default ShiftsAppliedFor
