6c0c200cf3065a8132b40371dfdabf945d6fcf72
admin_dho <davidhorph@hotmail.com>
2025-07-01 12:40:18 -0500

    Update SchedulingNeeds.jsx
    Inside customButtons.candidate.function: I added logic that so far, displays the data after the user clicks "Apply for Shift" button. the id argument (first argument) is now shiftID to add clarity (to distinguish it from user id).

--- src/components/shared/scheduling_needs/SchedulingNeeds.jsx
+++ src/components/shared/scheduling_needs/SchedulingNeeds.jsx
@@ -1,18 +1,41 @@
-import {useContext} from 'react'
+//Dependencies.
+import { useContext } from 'react'
 import { ShiftContext } from '../../../App';
+import { Button, Container } from 'reactstrap';
+import { putRequest } from '../../../functions/putRequest';
 
 import "./SchedulingNeeds.styles.css";
-import { Button, Container } from 'reactstrap';
 
 const SchedulingNeeds = () => {
-   const { shiftsArray, currentUser } = useContext(ShiftContext);
+   const { shiftsArray, setShiftsArray, currentUser, setCurrentUser, setEmployees, employees } = useContext(ShiftContext);
    const { role } = currentUser;
 
    const customButtons = {
       candidate: [
          {
             name: "APPLY FOR SHIFT!!!",
-            function: (id, store) => console.log(`Create logic to send notice to supervisor for ${id}, store ${store}.`)
+            function: (shiftID, store, ...args/* [candidate, shift] */) => {
+               const updatedCandidateData = Array.from(args)[0];
+               const { id: candidateID } = updatedCandidateData;
+               updatedCandidateData.shiftsAppliedFor ?
+                  updatedCandidateData.shiftsAppliedFor = [...updatedCandidateData.shiftsAppliedFor, shiftID]
+                  :
+                  updatedCandidateData.shiftsAppliedFor = [shiftID]; //Ternary operator to see if there is a property called shiftsAppliedFor.
+
+               const updatedShiftData = Array.from(args)[1];
+               updatedShiftData.applicants ?
+                  updatedShiftData.applicants = [...updatedCandidateData.applicants, updatedCandidateData]
+                  :
+                  updatedShiftData.applicants = [updatedCandidateData] //Ternary operator to see if there is an applicants property.
+               
+               const updatedEmployees = [...employees.filter(val => val.id != candidateID), updatedCandidateData];
+
+               const updatedShifts = [...shiftsArray.filter(val => val.id != shiftID), updatedShiftData]
+
+               console.log("===== HERE IS YOUR DATA!!! =====");
+               console.log({ shiftID, candidateID, store, updatedCandidateData, updatedShiftData, updatedEmployees, updatedShifts, args });
+               console.log("================================");
+            }
          },
          {
             name: "QUESTION ABOUT SHIFT",
@@ -29,7 +52,7 @@ const SchedulingNeeds = () => {
             function: (id, store) => console.log(`INSERT LOGIC TO VIEW ALL CANDIDATES FOR shift id ${id} - store ${store}.`)
          }
       ]
-   }
+   } //custom buttons that will be used for shitsArray.map.
 
    return (
       <div>
@@ -48,7 +71,7 @@ const SchedulingNeeds = () => {
                         {
                            customButtons[role]
                               .map((val, idx) => (
-                                 <Button key={idx} color= {idx%2==1 ? 'danger' : 'success'} size='sm' onClick={() => val.function(id, storeNumber)}><strong>{val.name}</strong></Button>
+                                 <Button key={idx} color= {idx%2==1 ? 'danger' : 'success'} size='sm' onClick={() => val.function(id, storeNumber, {...currentUser}, shiftsArray.filter(val => val.id == id)[0])}><strong>{val.name}</strong></Button>
                               ))
                         }
                      </div>
