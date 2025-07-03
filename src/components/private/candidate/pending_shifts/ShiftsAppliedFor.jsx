64bc8382d83e66813d89c5d7b65959b1bceb633f
admin_dho <davidhorph@hotmail.com>
2025-07-03 13:15:38 -0500

    filtered and displayed the shifts signed up.
    Replaced shiftsAppliedFor.map( ) with shiftsArray.filter( ).map( ) because, what I want to do is to: 1. FILTER out the shifts in the shiftsArray state that are not inluded in the array of shiftsAppliedFor (the shifts I checked). Then... 2. I want to map it out.
    
    I also changed the result so that the shift details show instead of just the id.

--- src/components/private/candidate/pending_shifts/ShiftsAppliedFor.jsx
+++ src/components/private/candidate/pending_shifts/ShiftsAppliedFor.jsx
@@ -7,13 +7,13 @@ import { Container } from 'reactstrap';
 import "./ShiftsAppliedFor.styles.css";
 
 const ShiftsAppliedFor = () => {
-  const { currentUser } = useContext(ShiftContext);
+  const { currentUser, shiftsArray } = useContext(ShiftContext);
   const { shiftsAppliedFor } = currentUser; //array of shifts.
 
   if (!shiftsAppliedFor) {
     return (
       <div>
-        <Container className='no-shifts-container'>
+        <Container className='p-1'>
           <h1>You Have Not Applied for any Shifts (yet)!!!</h1>
         </Container>
       </div>
@@ -26,13 +26,16 @@ const ShiftsAppliedFor = () => {
       </header>
       <Container>
          {
-          shiftsAppliedFor.map((val, idx) => {
-            return (
-              <div key={idx}>
-                <strong>{JSON.stringify(val)}</strong>
+          shiftsArray
+            .filter(({ id }) => shiftsAppliedFor.includes(id))
+            .map(({ id, storeNumber, date, time }, idx) => (
+              <div className={`p-1 my-3 ${idx%2==1 ? 'shift-cell-1' : 'shift-cell-2'}`} key={idx}>
+                <h5>shift id:<span class='text-danger'>{id}</span></h5>
+                <h5>store number:<span class='text-danger'>{storeNumber}</span></h5>
+                <h5>shift schedule:<span class='text-danger'>{date}</span></h5>
+                <h5>shift time:<span class='text-danger'>{time}</span></h5>
               </div>
-            )
-          })
+            ))
          }
       </Container>
     </div>
