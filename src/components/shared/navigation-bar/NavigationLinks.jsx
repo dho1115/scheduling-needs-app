39fe3a15f4e894d440062410248848cacdca77bf
admin_dho <davidhorph@hotmail.com>
2025-07-22 19:29:33 -0500

    Update NavigationLinks.jsx
    New Routes added.

--- src/components/shared/navigation-bar/NavigationLinks.jsx
+++ src/components/shared/navigation-bar/NavigationLinks.jsx
@@ -4,9 +4,10 @@ export const NavigationLinks = (objects = null /* {role: ... } */) => {
          { name: "Login Page", to: "/", restrictions: null },
          { name: "View Available Shifts", to: `/${objects.role}/welcome/${objects.id}/available shifts`, restrictions: null },
          { name: "Add Shift", to: `/${objects.role}/welcome/${objects.id}/add shift`, restrictions: 'supervisor' },
-         {name: "Shifts Applied For", to: `/candidate/welcome/${objects.id}/shifts/applied`, restrictions: 'candidate'},
-         {name: "Shifts Awarded", to: `/candidate/welcome/${objects.id}/shifts/awarded`, restrictions: 'candidate'},
-         {name: "Shifts With Applicants", to: `/supervisor/welcome/${objects.id}/shifts/applied`, restrictions: 'supervisor'},
+         { name: "Shifts Applied For", to: `/candidate/welcome/${objects.id}/shifts/applied`, restrictions: 'candidate' },
+         { name: "Shifts Awarded", to: `/candidate/welcome/${objects.id}/shifts/awarded`, restrictions: 'candidate' },
+         { name: "Shifts With Applicants", to: `/supervisor/welcome/${objects.id}/shifts/applied`, restrictions: 'supervisor' },
+         { name: "Assigned Shifts", to: `/supervisor/welcome/${objects.id}/shifts/awarded`, restrictions: 'supervisor' },
          { name: "Cancel Available Shift", to: "/", restrictions: 'supervisor' }
       ]
    } catch (error) {
