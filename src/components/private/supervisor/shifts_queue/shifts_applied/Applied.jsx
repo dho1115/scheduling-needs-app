a6c4f273666c4d0058862ae8a02a97f0eb74420d
admin_dho <davidhorph@hotmail.com>
2025-09-07 16:35:27 -0500

    Put, Post functionality, setState.
    Sucessfully added Put, Post request so that:
    1. The candidate can select the shift.
    2. The supervisor can award the shift.
    3. The awarded shift goes into a queue where that AWAITS for the candidate to confirm it:
    http://localhost:3001/supervisor/welcome/s-mc9rq8m6/shifts/unconfirmed-shifts
    4. Meanwhile, the supervisor can go to his/her/their queue to see which awarded shifts still need the candidate to confirm:
    http://localhost:3001/supervisor/welcome/s-mc9rq8m6/shifts/unconfirmed-shifts

--- src/components/private/supervisor/shifts_queue/shifts_applied/Applied.jsx
+++ src/components/private/supervisor/shifts_queue/shifts_applied/Applied.jsx
@@ -29,7 +29,8 @@ const Applied = () => {
 
       Promise.all([
          PostRequest('http://localhost:3003/shiftsPendingEmployeeConfirm', { id: shiftID, storeNumber, _candidateID: id, candidate: name, approvedBy: currentUser.id }),
-         PutRequest('http://localhost:3003/employees', updatedEmployees)
+         PutRequest('http://localhost:3003/employees', id, updatedEmployees),
+         DeleteShift(`http://localhost:3003/availableShifts/${shiftID}`, shiftID)
       ])
          .then(result => {
             console.log({ message: 'PROMISE.ALL SUCCESS!!!', result });
@@ -45,70 +46,57 @@ const Applied = () => {
          .catch(error => console.error({ message: "PROMISE.ALL ERROR!!!", error, errorCode: error.code, errorMessage: error.message }));
    }
 
-      // Promise.all([ PostRequest('http://localhost:3003/shiftsPendingEmployeeConfirm', shiftID), PutRequest('http://localhost:3003/employees', updatedEmployees), DeleteShift(`http://localhost:3003/availableShifts/${shiftID}`, shiftID) ])
-      //    .then(result => {
-      //       console.log({ message: 'PROMISE.ALL SUCCESS!!!', result });
-      //       setEmployees(updatedEmployees);
-      //       setShiftsArray(updateAvailableShifts);
-      //       setUnconfirmedShifts(prv => [...prv, shiftID]);
-      //       return { message: "Finished setting state for employees ShiftsArray, UnfonfirmedShifts", result, updatedState: { unconfirmedShifts, employees, shiftsArray } }
-      //    }).then(message => {
-      //       console.log(message);
-      //       navigate(`http://localhost:3001/supervisor/welcome/${currentUser.id}/shifts/unconfirmed-shifts`);
-      //    })
-      //    .catch(error => console.error({ message: "PROMISE.ALL ERROR!!!", error, errorCode: error.code, errorMessage: error.message }));
-      return (
-         <div>
-            <header className='applied-for-header p-1'>
-               <h1>SHIFT(S) WITH CANDIDATES!!!</h1>
-            </header>
+   return (
+      <div>
+         <header className='applied-for-header p-1'>
+            <h1>SHIFT(S) WITH CANDIDATES!!!</h1>
+         </header>
 
-            <Container className='applied-for-shifts p-3 m-3'>
-               {
-                  appliedForShifts.length ?
-                     appliedForShifts.map(({ id, date, time, storeNumber, applicants }, idx) => {
-                        const shiftID = id;
-                        return (
-                           <div className='p-3 m-1' style={{ border: '1.5px solid black', backgroundColor: idx % 2 == 1 ? 'lightyellow' : 'antiquewhite' }} key={idx}>
-                              <h5>id: <span style={{color: 'firebrick'}}>{shiftID}</span></h5>
-                              <h5>date: <span style={{color: 'firebrick'}}>{date}</span></h5>
-                              <h5>time: <span style={{color: 'firebrick'}}>{time}</span></h5>
-                              <h3>store: <span style={{ color: 'firebrick' }}>{storeNumber}</span></h3>
-                              <hr />
-                              <h5>APPLICANTS:</h5>
-                              {
-                                 applicants.map(id => {
-                                    const employee = employees.find(val => id == val.id);
+         <Container className='applied-for-shifts p-3 m-3'>
+            {
+               appliedForShifts.length ?
+                  appliedForShifts.map(({ id, date, time, storeNumber, applicants }, idx) => {
+                     const shiftID = id;
+                     return (
+                        <div className='p-3 m-1' style={{ border: '1.5px solid black', backgroundColor: idx % 2 == 1 ? 'lightyellow' : 'antiquewhite' }} key={idx}>
+                           <h5>id: <span style={{color: 'firebrick'}}>{shiftID}</span></h5>
+                           <h5>date: <span style={{color: 'firebrick'}}>{date}</span></h5>
+                           <h5>time: <span style={{color: 'firebrick'}}>{time}</span></h5>
+                           <h3>store: <span style={{ color: 'firebrick' }}>{storeNumber}</span></h3>
+                           <hr />
+                           <h5>APPLICANTS:</h5>
+                           {
+                              applicants.map(id => {
+                                 const employee = employees.find(val => id == val.id);
 
-                                    return (
-                                       <div key={id} className='applicant-div p-1 m-1'>
-                                          <h5>id: {id}</h5>
-                                          <p><strong>name: {employee.name}</strong></p>
-                                          <p><strong>base: {employee.base}</strong></p>
-                                          <button
-                                             className='w-100 btn btn-success'
-                                             onClick={() => onHandleAwardShift(employee.name, id, shiftID, storeNumber)}
-                                          >
-                                             ASSIGN SHIFT TO {employee.name.toUpperCase()}!!!
-                                          </button>
-                                       </div>
-                                    )
-                                 })
-                              }
-                           </div>
-                        )
-                     })
-                     :
-                     <>
-                        <h1>SORRY... NOBODY HAS APPLIED FOR <i>ANY</i> SHIFT...</h1>
-                        <h5>Bunch of fucking lazy asses!!!</h5>
-                        <h5>Either that, or they're just too busy coding, writing screenplays or... fuckin' lazy asses!!!</h5>
-                     </>
-               }
-            </Container>
-         </div>
-      )
-   }
-
+                                 return (
+                                    <div key={id} className='applicant-div p-1 m-1'>
+                                       <h5>id: {id}</h5>
+                                       <p><strong>name: {employee.name}</strong></p>
+                                       <p><strong>base: {employee.base}</strong></p>
+                                       <button
+                                          className='w-100 btn btn-success'
+                                          onClick={() => onHandleAwardShift(employee.name, id, shiftID, storeNumber)}
+                                       >
+                                          ASSIGN SHIFT TO {employee.name.toUpperCase()}!!!
+                                       </button>
+                                    </div>
+                                 )
+                              })
+                           }
+                        </div>
+                     )
+                  })
+                  :
+                  <>
+                     <h1>SORRY... NOBODY HAS APPLIED FOR <i>ANY</i> SHIFT...</h1>
+                     <h5>Bunch of fucking lazy asses!!!</h5>
+                     <h5>Either that, or they're just too busy coding, writing screenplays or... fuckin' lazy asses!!!</h5>
+                  </>
+            }
+         </Container>
+      </div>
+   )
+}
 
 export default Applied
