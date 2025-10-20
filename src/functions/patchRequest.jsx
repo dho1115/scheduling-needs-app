70bee32c2320840d461a4dc2623b7c7c6d9e6405
admin_dho <davidhorph@hotmail.com>
2025-10-20 09:52:13 -0500

    Create patchRequest.jsx
    PatchRequest to make change to a single property of an object (as opposed to PUT which replaces the entire object) and PatchDBandState which updates db and the state.

---
+++ src/functions/patchRequest.jsx
@@ -0,0 +1,18 @@
+export const PatchRequest = (url, newData) => fetch(url, {
+   method: 'PATCH',
+   headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Accept': 'application/json'
+   },
+   body: JSON.stringify(newData)
+})
+
+export const PatchDBandState = async (PatchRequest, setStateWrapper, url, newDataObject, ...args) => {
+   try {
+      const patchDB = await PatchRequest(url, newDataObject)
+      setStateWrapper();
+      return { message: "PATCH SUCCESS!!!", patchDB, url, newDataObject, setStateWrapper };
+   } catch (error) {
+      console.error({ message: 'PatchDBandState ERROR!!!', error, errorCode: error.code, errorMessage: error.message });
+   }
+}
