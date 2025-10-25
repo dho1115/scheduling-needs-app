0e3431163710b944e0d658836f5a2b5805ea0384
admin_dho <davidhorph@hotmail.com>
2025-10-25 13:53:15 -0500

    Update postRequest.js
    New function that adds new shift to DB and sets state.

--- src/functions/postRequest.js
+++ src/functions/postRequest.js
@@ -2,3 +2,11 @@ export const PostRequest = (url, body) => {
    return fetch(url, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(body) })
 }
 
+export const AddNewShiftToDBandState = (url, data, setStateWrapperFunction /*  (data) => setState(...) */, location = null, ...args) => PostRequest(url, data)
+   .then(resultOfPost => {
+      console.log({ message: "AddNewShiftToDBandState SUCCESSFULL!!!", resultOfPost });
+
+      return setStateWrapperFunction(data);
+   })
+   .catch(error => console.error({ location, message: "Error with AddNewShiftToDBandState!!!", error, errorCode: error.code, errorMessage: error.message }));
+
