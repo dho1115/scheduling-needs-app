d6a07914d3230fddcdda86f5dbee4f4e946b1025
admin_dho <davidhorph@hotmail.com>
2025-10-25 11:37:24 -0500

    Create LoadingComponent.jsx

---
+++ src/components/LoadingComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LoadingComponent = () => {
+  return (
+    <div>
+      <Container style={{textAlign: 'center', backgroundColor: 'beige'}}>
+         <h1>LOADING... PLEASE WAIT</h1>
+      </Container>
+    </div>
+  )
+}
+
+export default LoadingComponent
