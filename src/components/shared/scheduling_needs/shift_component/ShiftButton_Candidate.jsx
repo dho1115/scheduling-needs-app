034a8ae1a88fd060f5df8fe68bf022072676fc5f
admin_dho <davidhorph@hotmail.com>
2025-10-28 14:50:01 -0500

    Update ShiftButton_Candidate.jsx
    Changed to did candidate apply for shift.

--- src/components/shared/scheduling_needs/shift_component/ShiftButton_Candidate.jsx
+++ src/components/shared/scheduling_needs/shift_component/ShiftButton_Candidate.jsx
@@ -8,25 +8,29 @@ import { ShiftContext } from '../../../../App';
 import { Button } from 'reactstrap';
 
 //functions.
-import { findShiftInArray } from '../functions';
+import { didApplicantApplyForShift, findShiftInArray } from '../functions';
 import { PostRequestSetState } from '../../../../functions/postRequest';
 
 import "./Shift.styles.css";
 
 const CandidateShiftButtons = (props) => {
-  const { setModal, toggle, currentUser: { password, ...otherUserDetails }, ...otherProps } = props;
   const navigate = useNavigate();
+  const { setModal, toggle, currentUser: { password, ...otherUserDetails }, ...otherProps } = props;
+  const { id, shiftID } = otherProps;
   const { shiftStatuses, setShiftStatuses } = useContext(ShiftContext);
   const { shiftsWithApplicants, shiftsAvailable } = shiftStatuses;
+
+  console.log({ shiftsWithApplicants });
   
   const { id: _currentUserID, name } = props.currentUser;
 
   const onSelectShift = async () => {
     const shiftAndCandidate = { ...otherProps, applicant: otherUserDetails };
+
     try {
       const addToShiftsWithApplicants = await PostRequestSetState("http://localhost:3003/shiftsWithApplicants", { ...shiftAndCandidate }, () => setShiftStatuses(prv => ({ ...prv, shiftsWithApplicants: [...prv.shiftsWithApplicants, { ...shiftAndCandidate }] })));
 
-      console.log({ message: "POST IS SUCCESSFUL!!!", data: { ...otherProps, candidate: otherUserDetails } });
+      console.log({ message: "POST IS SUCCESSFUL!!!", data: { ...otherProps, candidate: otherUserDetails, shiftsWithApplicants } });
       
       navigate(`/candidate/welcome/${_currentUserID}/available shifts`)
 
@@ -38,7 +42,7 @@ const CandidateShiftButtons = (props) => {
 
   return (
     <div className='shiftButton_div_candidate'>
-      <Button size='lg' className='m-1' color={findShiftInArray(props.shiftID, shiftsWithApplicants) ? 'secondary' : 'danger'} onClick={onSelectShift} disabled={ findShiftInArray(props.shiftID, shiftsWithApplicants) }>{ findShiftInArray(props.shiftID, shiftsWithApplicants) ? "SELECTED" : "SELECT"}</Button>
+      <Button size='lg' className='m-1' color={didApplicantApplyForShift(_currentUserID, shiftID, shiftsWithApplicants) ? 'secondary' : 'danger'} onClick={onSelectShift} disabled={ didApplicantApplyForShift(_currentUserID, shiftID, shiftsWithApplicants) }>{ didApplicantApplyForShift(_currentUserID, shiftID, shiftsWithApplicants) ? "SELECTED" : "SELECT"}</Button>
       
       <Button size='sm' className='p-0' color='success' onClick={toggle}>QUESTION ABOUT SHIFT</Button>
     </div>
