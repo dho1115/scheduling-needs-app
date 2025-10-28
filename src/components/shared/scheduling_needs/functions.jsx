ae5ec85d93bde91e9b93aaa311e2eaa0ec5a0c77
admin_dho <davidhorph@hotmail.com>
2025-10-27 19:59:51 -0500

    New Functions: TransferShift, FindCandidates.

---
+++ src/components/shared/scheduling_needs/functions.jsx
@@ -0,0 +1,15 @@
+import { DeleteRequestSetState } from "../../../functions/deleteRequest"
+import { PostRequestII, PostRequestSetState } from "../../../functions/postRequest"
+
+export const TransferShift = async (fromURL, toURL, postData, postStateWrapper, deleteStateWrapper) => {
+   try {
+      const postDataLogic = await PostRequestSetState(toURL, postData, postStateWrapper);
+      const deleteDataLogic = await DeleteRequestSetState(fromURL, deleteStateWrapper);
+
+      return postData;
+   } catch (error) {
+      console.error({ message: "ERROR inside TransferShift function!!!", error, errorMessage: error.message, errorCode: error.code, errorStack: error.stack });
+   }
+}
+
+export const findCandidateInShiftStatus = (_candidateID, shiftStatus) => shiftStatus.find(({ currentUser: { id } }) => id == _candidateID);
