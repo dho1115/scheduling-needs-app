0ae12376239c5b50bc9ca73d70d119779844f0f2
admin_dho <davidhorph@hotmail.com>
2025-10-27 23:21:47 -0500

    Update postRequest.js

--- src/functions/postRequest.js
+++ src/functions/postRequest.js
@@ -12,3 +12,20 @@ export const AddNewShiftToDBandState = (url, data, setStateWrapperFunction /*  (
    })
    .catch(error => console.error({ location, message: "Error with AddNewShiftToDBandState!!!", error, errorCode: error.code, errorMessage: error.message }));
 
+export const PostRequestII = async (url, data, id = null) => {
+   try {
+      const post = await fetch(url, { method: 'POST', headers: { 'Content-Type': "application/json" }, body: JSON.stringify(data) });
+
+      return data;
+   } catch (error) {
+      console.error({ message: "PostRequest Error!!!", error, errorMessage: error.message, errorStack: error.stack });
+   }
+}
+
+export const PostRequestSetState = (url, data, setStateWrapper) => PostRequestII(url, data)
+   .then(data => {
+      setStateWrapper();
+      return data;
+   })
+   .catch(error => console.error({ message: "PostRequestSetState ERROR!!!", error, errorMessage: error.message, errorStack: error.stack, errorCode: error.code }));
+
