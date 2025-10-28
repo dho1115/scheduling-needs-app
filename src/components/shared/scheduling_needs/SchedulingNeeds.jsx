9b778230e6ac52c5954596507f67defa2cbfc5a4
admin_dho <davidhorph@hotmail.com>
2025-10-27 23:21:15 -0500

    Update SchedulingNeeds.jsx

--- src/components/shared/scheduling_needs/SchedulingNeeds.jsx
+++ src/components/shared/scheduling_needs/SchedulingNeeds.jsx
@@ -1,16 +1,17 @@
 //Dependencies.
-import { Suspense } from 'react';
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { ShiftContext } from '../../../App';
 import Shift from './shift_component/Shift';
 import { Container } from 'reactstrap';
-import { PutRequest } from '../../../functions/putRequest';
-import { DateTime } from 'luxon';
 
 import "./SchedulingNeeds.styles.css";
 
 const SchedulingNeeds = () => {
-   const { shiftStatuses: { shiftsAvailable } } = useContext(ShiftContext);
+   const { shiftStatuses, shiftStatuses: { shiftsAvailable } } = useContext(ShiftContext);
+
+   useEffect(() => {
+      console.log({ shiftStatuses });
+   }, [])
 
    return (
       <div>
@@ -19,7 +20,7 @@ const SchedulingNeeds = () => {
          </header>
          <Container className='p-3 scheduling-needs-container'>
             {
-               shiftsAvailable.map((val, idx) => <Shift {...val} idx={idx}/>)
+               shiftsAvailable.map((val, idx) => <Shift key={idx} {...val} idx={idx}/>)
             }
          </Container>
       </div>
