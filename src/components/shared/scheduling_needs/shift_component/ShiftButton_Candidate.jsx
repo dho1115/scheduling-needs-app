01d8867e427e50532bce3608b71ce15a59701cae
admin_dho <davidhorph@hotmail.com>
2025-10-28 20:08:35 -0500

    new shiftWithApplicantID
    This is the new, UNIQUE id for each shiftWithApplicant object and will be a concatenation of the currentUserID and shiftID.
    
    I borrowed this from the Unique( kw1, kw2 ) constraint I learned from Postgres.

--- src/components/shared/scheduling_needs/shift_component/ShiftButton_Candidate.jsx
+++ src/components/shared/scheduling_needs/shift_component/ShiftButton_Candidate.jsx
@@ -16,16 +16,17 @@ import "./Shift.styles.css";
 const CandidateShiftButtons = (props) => {
   const navigate = useNavigate();
   const { setModal, toggle, currentUser: { password, ...otherUserDetails }, ...otherProps } = props;
-  const { id, shiftID } = otherProps;
+  const { id, shiftID, ...remainingProps } = otherProps;
   const { shiftStatuses, setShiftStatuses } = useContext(ShiftContext);
-  const { shiftsWithApplicants, shiftsAvailable } = shiftStatuses;
-
-  console.log({ shiftsWithApplicants });
+  const { shiftsWithApplicants } = shiftStatuses;
   
   const { id: _currentUserID, name } = props.currentUser;
 
+  const alreadyApplied = didApplicantApplyForShift(_currentUserID, shiftID, shiftsWithApplicants) //true or false;
+
   const onSelectShift = async () => {
-    const shiftAndCandidate = { ...otherProps, applicant: otherUserDetails };
+    const shiftWithApplicantID = `${_currentUserID}-${shiftID}`
+    const shiftAndCandidate = { id: shiftWithApplicantID, shiftID, applicant: otherUserDetails, ...remainingProps };
 
     try {
       const addToShiftsWithApplicants = await PostRequestSetState("http://localhost:3003/shiftsWithApplicants", { ...shiftAndCandidate }, () => setShiftStatuses(prv => ({ ...prv, shiftsWithApplicants: [...prv.shiftsWithApplicants, { ...shiftAndCandidate }] })));
@@ -42,9 +43,9 @@ const CandidateShiftButtons = (props) => {
 
   return (
     <div className='shiftButton_div_candidate'>
-      <Button size='lg' className='m-1' color={didApplicantApplyForShift(_currentUserID, shiftID, shiftsWithApplicants) ? 'secondary' : 'danger'} onClick={onSelectShift} disabled={ didApplicantApplyForShift(_currentUserID, shiftID, shiftsWithApplicants) }>{ didApplicantApplyForShift(_currentUserID, shiftID, shiftsWithApplicants) ? "SELECTED" : "SELECT"}</Button>
+      <Button size='lg' className='m-1' color={alreadyApplied ? 'secondary' : 'danger'} onClick={onSelectShift} disabled={alreadyApplied}>{ alreadyApplied ? "APPLIED" : "APPLY"}</Button>
       
-      <Button size='sm' className='p-0' color='success' onClick={toggle}>QUESTION ABOUT SHIFT</Button>
+      <Button size='sm' className='m-1' color='success' onClick={toggle}>QUESTION ABOUT SHIFT</Button>
     </div>
   )
 }
