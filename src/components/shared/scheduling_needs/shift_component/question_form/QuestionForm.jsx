7b16135fcb6f95d2ee83aeda3107fcba790e451d
admin_dho <davidhorph@hotmail.com>
2025-10-28 20:05:38 -0500

    Update QuestionForm.jsx
    Question Form handle submit. Doesn't sent to email.

--- src/components/shared/scheduling_needs/shift_component/question_form/QuestionForm.jsx
+++ src/components/shared/scheduling_needs/shift_component/question_form/QuestionForm.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Form, FormGroup, Input, Label } from 'reactstrap'
 
-const QuestionForm = () => {
-   
+const QuestionForm = (props) => {
+   const [questionForm, setQuestionForm] = useState({submitted_by: `${props.name} (id# ${props.id})`, shiftID: props.shiftID, store: props.storeNumber, title: '', body: '' });
+
+   const onHandleSubmit = e => {
+      e.preventDefault();
+      try {
+         console.log("===== YOUR QUESTION DETAILS: =====");
+         console.log(JSON.stringify(questionForm));
+         console.log("==================================")
+         alert(`Your Question:\n ${JSON.stringify(questionForm)}.`)
+      } catch (error) {
+         console.error({ message: "QuestionForm Error!!!", error, errorMessage: error.message });
+      }
+   }
    return (
-      <Form>
+      <Form onSubmit={onHandleSubmit}>
          <FormGroup>
             <Label for='title'>TITLE</Label>
-            <Input type='text' id='title' placeholder='TITLE OF YOUR QUESTION.' />
+            <Input type='text' id='title' value={questionForm.title} placeholder='TITLE OF YOUR QUESTION.' onChange={e => setQuestionForm(prv => ({...prv, title: e.target.value}))}/>
          </FormGroup>
          <FormGroup>
             <Label for='body'>MESSAGE</Label>
-            <Input type='textarea' id='body' placeholder='Your Message Here.' max={195} />
+            <Input type='textarea' id='body' value={questionForm.body} placeholder='Your Message Here.' max={195} onChange={e => setQuestionForm(prv => ({...prv, body: e.target.value}))} />
          </FormGroup>
          <FormGroup>
-            <Button size='md' className='w-100' color='danger'>SUBMIT!!!</Button>
+            <Button type='submit' size='md' className='w-100' color='danger'>SUBMIT!!!</Button>
          </FormGroup>
       </Form>
    )
