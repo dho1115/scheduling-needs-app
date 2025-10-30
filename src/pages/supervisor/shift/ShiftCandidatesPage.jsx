7f3a59d47acaec1ef6e355c81969d709e156304f
admin_dho <davidhorph@hotmail.com>
2025-10-29 22:26:20 -0500

    Update ShiftCandidatesPage.jsx

--- src/pages/supervisor/shift/ShiftCandidatesPage.jsx
+++ src/pages/supervisor/shift/ShiftCandidatesPage.jsx
@@ -20,6 +20,8 @@ const ShiftCandidatesPage = (props) => {
 
   const thisShiftDetails = findShiftInArray(_shiftID, shiftsWithApplicants);
 
+  const onApproveRequest = (shiftID, name) => console.log(`REQUEST FOR SHIFT# ${shiftID} APPROVED!!!\n TRANSFERRING TO shiftsPendingConfirmation.\n WAITING FOR ${name}'S LAZY ASS TO CONFIRM IT!!!`)
+
   return (
     <div>
       <header className='my-5'>
@@ -34,7 +36,9 @@ const ShiftCandidatesPage = (props) => {
                 <h3>EMPLOYEE_ID: <span className='text-danger'>{id}</span></h3>
                 <h3>BASE STORE: <span className='text-danger'>{base}</span></h3>
                 <div className='w-100'>
-                  <Button className='w-100' size='md' color= {idx%2==1 ? 'danger' : 'success'}>SELECT THIS EMPLOYEE FOR SHIFT #{_shiftID}</Button>
+                  <Button className='w-100' size='md' color={idx % 2 == 1 ? 'success' : 'danger'} onClick={() => onApproveRequest(_shiftID, name)}>
+                    <strong>APPROVE {name}'S REQUEST FOR SHIFT #{_shiftID}!!!</strong>
+                  </Button>
                 </div>
               </div>
             ))
