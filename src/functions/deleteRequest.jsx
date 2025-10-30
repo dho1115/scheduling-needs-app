220474fa286d91ea655878661e3f07ab8fb4d7b3
admin_dho <davidhorph@hotmail.com>
2025-10-30 12:10:19 -0500

    Added DeleteBatch
    Deletes a bunch of objects from db.

--- src/functions/deleteRequest.jsx
+++ src/functions/deleteRequest.jsx
@@ -1,5 +1,19 @@
-export const DeleteRequest = (url, id=null) => {
+export const DeleteRequest = (url, id = null) => {
    return fetch(url, { method: 'DELETE' });
+}
+
+export const DeleteBatch = (...args_to_delete) => {
+   try {
+      const convertArrayToItems = Array.from(args_to_delete).forEach(item => {
+         if (typeof (item) == 'object' && item.length) throw new Error(`${JSON.stringify(item)} is an array!!! please enter all arrays as spread operators!!!`)
+         
+         return Promise.all(Array.from(args_to_delete).map(async item => await DeleteRequest(item)));
+      });
+   } catch (error) {
+      console.error({ message: "ERROR DELETING BATCH!!!", error, errorMessage: error.message, errorStack: error.stack, errorCode: error.code });
+      
+      return error;
+   }
 }
 
 export const DeleteRequestSetState = async (url, setStateWrapper, data) => {
