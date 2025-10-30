960c81c5e5263ad5769cc84b30cda2077c950400
admin_dho <davidhorph@hotmail.com>
2025-10-30 09:53:59 -0500

    Renaming Links to make more intuitive.

--- src/components/shared/navigation-bar/NavigationLinks.jsx
+++ src/components/shared/navigation-bar/NavigationLinks.jsx
@@ -5,8 +5,8 @@ export const NavigationLinks = (objects = null /* {role: ... } */, ...args) => {
          { name: "View Available Shifts", to: `/${objects.role}/welcome/${objects.id}/available shifts`, restrictions: "candidate" },
          Array.from(args).includes("candidates") ? { name: "YOU'VE GOT CANDIDATES!!!", to: `/${objects.role}/welcome/${objects.id}/available shifts`, restrictions: "supervisor" } : { name: "OPEN SHIFTS", to: `/${objects.role}/welcome/${objects.id}/available shifts`, restrictions: "supervisor" },
          { name: "Add Shift", to: `/${objects.role}/welcome/${objects.id}/add shift`, restrictions: 'supervisor' },
-         { name: "Shifts Applied For", to: `/candidate/welcome/${objects.id}/shifts/applied`, restrictions: 'candidate' },
-         {name: "Shifts Needing Confirmation", to: `/candidate/welcome/${objects.id}/shifts/pending confirmation`, restrictions: 'candidate'},
+         { name: "Shifts Pending Approval/Denial", to: `/candidate/welcome/${objects.id}/shifts/applied`, restrictions: 'candidate' },
+         {name: "Approved Shifts Needing My Confirmation", to: `/candidate/welcome/${objects.id}/shifts/pending confirmation`, restrictions: 'candidate'},
          { name: "unconfirmed shifts", to: `/supervisor/welcome/${objects.id}/shifts/unconfirmed-shifts`, restrictions: 'supervisor'},
          { name: "Cancel Available Shift", to: "/", restrictions: 'supervisor' }
       ]
