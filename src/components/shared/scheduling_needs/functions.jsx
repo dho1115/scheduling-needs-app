b26cd31698856e4437103f2b39c3f7735c5ce03a
admin_dho <davidhorph@hotmail.com>
2025-10-30 21:26:29 -0500

    TransferShift function.
    New (updated) function that handles transferring shift from shiftWithApplicants to shiftsPendingConfirmation.
    
    Also, new function: didApplicantApplyForShift. Checks if the shiftWithApplicant object contains an applicant who applied for that shift via matching shiftWithApplicant.id with ${candidate ID} - ${shift ID}. This is what shiftWithApplicantID is made of.

--- src/components/shared/scheduling_needs/functions.jsx
+++ src/components/shared/scheduling_needs/functions.jsx
@@ -1,17 +1,27 @@
 import { DeleteRequestSetState } from "../../../functions/deleteRequest"
-import { PostRequestII, PostRequestSetState } from "../../../functions/postRequest"
+import { AddNewShiftToDBandState, PostRequestII, PostRequestSetState } from "../../../functions/postRequest"
+AddNewShiftToDBandState
 
-export const TransferShift = async (fromURL, toURL, postData,dataToPostArray, postStateWrapper, dataToDeleteArray, deleteStateWrapper) => {
+export const TransferApprovedShift = async (approvedShiftWithApplicant, applicant, shiftid, formattedDateApproved /* DateTime API */, currentUser, setStateWrapper, location=null) => {
    try {
-      const postDataLogic = await PostRequestSetState(toURL, postData, postStateWrapper);
-      const deleteDataLogic = await DeleteRequestSetState(fromURL, deleteStateWrapper);
+      if (!approvedShiftWithApplicant) throw new Error(`*** NO APPROVED SHIFT WITH APPLICANT!!!\nThe function, approvedShiftWithApplicant returned:\n ${approvedShiftWithApplicant}. LOCATION OF ERROR => ${location}`);
 
-      return postData;
+      const approved_shift_updated = { ...approvedShiftWithApplicant, approvedOn: formattedDateApproved, approvedBy: currentUser.name, supervisorId: currentUser.id }; //approved shift bject be tranferred to shiftsPendingConfirmation.
+      
+      return await AddNewShiftToDBandState(
+         "http://localhost:3003/shiftsPendingConfirmation", approved_shift_updated,
+         setStateWrapper(approved_shift_updated)
+      );
    } catch (error) {
-      console.error({ message: "ERROR inside TransferShift function!!!", error, errorMessage: error.message, errorCode: error.code, errorStack: error.stack });
+      console.error({ message: "TransferShift function error!!!", error, errorStack: error.stack, errorName: error.name, errorMessage: error.message, errorCode: error.code });
    }
 }
 
-export const findShiftInArray = (id_of_shift, array) => array.find(({ shiftID }) => shiftID == id_of_shift);
+export const findShiftInArray = (id_of_shift, array) => {
+   console.log(id_of_shift, array);
+   return array.filter(({ shiftID }) => shiftID == id_of_shift)
+};
 
-export const didApplicantApplyForShift = (_currentUserID, shiftID, shiftsWithApplicants) => shiftsWithApplicants.find(({id}) => (id.includes(_currentUserID)) && (id.includes(shiftID)))
+export const didApplicantApplyForShift = (_currentUserID, shiftID, shiftsWithApplicants) => shiftsWithApplicants.find(({ id }) => (id.includes(_currentUserID)) && (id.includes(shiftID)))
+
+export const youveGotApplicants = (shiftID_toFind, shiftsWithApplicants) => shiftsWithApplicants.filter(shiftWithApplicant => shiftWithApplicant.shiftID == shiftID_toFind)
