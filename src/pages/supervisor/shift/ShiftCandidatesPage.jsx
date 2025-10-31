dfbb1aa0d88c39602087a29a95652854f668742d
admin_dho <davidhorph@hotmail.com>
2025-10-31 00:29:27 -0500

    Fixed applicantsForThisShift.map( ).
    applicantsForThisShift.map(({ id, applicant }, idx) => ( ... ) # should have returned an applicant object AND and id
    
    BECAUSE...
    
    The shiftWithApplicantObject argument (the second argument inside onApproveRequest) EXPECTS the shiftWithApplicantObject parameter to be an object that has two properties: applicant and id.
    
    Thus, I had to:
    
    onApproveRequest(_shiftID, { applicant, id })
    
    I also attached the .then( ).catch( ) for navigation (.then( )) and error handling.

--- src/pages/supervisor/shift/ShiftCandidatesPage.jsx
+++ src/pages/supervisor/shift/ShiftCandidatesPage.jsx
@@ -9,7 +9,6 @@ import { Button, Container } from 'reactstrap';
 import { ShiftContext } from '../../../App'
 
 //functions.
-import { AddNewShiftToDBandState } from '../../../functions/postRequest';
 import { DeleteRequest } from '../../../functions/deleteRequest';
 import { FetchDataSetState } from '../../../functions/FetchHook';
 import { DeleteApprovedShift, findShiftInArray, TransferApprovedShift } from '../../../components/shared/scheduling_needs/functions';
@@ -18,6 +17,7 @@ import "./ShiftCandidatesPage.styles.css"
 
 const ShiftCandidatesPage = () => {
   const { pathname } = useLocation();
+  const navigate = useNavigate();
   const { currentUser, setShiftStatuses, shiftStatuses } = useContext(ShiftContext);
   const { _shiftID } = useParams();
 
@@ -26,25 +26,25 @@ const ShiftCandidatesPage = () => {
   const dateApproved = DateTime.local()
   const formattedDateApproved = dateApproved.toFormat("yyyy-MM-dd");
 
-  const applicantsForThisShift = shiftsWithApplicants.filter(({ shiftID }) => shiftID == _shiftID).map(({ applicant }) => applicant);
+  const applicantsForThisShift = shiftsWithApplicants.filter(({ shiftID }) => shiftID == _shiftID)
 
   const thisShiftWithApplicants = findShiftInArray(_shiftID, shiftsWithApplicants);
 
-  const onApproveRequest = async (shiftID, shiftWithApplicantObject) => {
-    const { id, _shiftID:shiftid, applicant } = shiftWithApplicantObject;
+  const onApproveRequest = async (_shiftID, shiftWithApplicantObject) => {
+    const { id, applicant } = shiftWithApplicantObject;
 
     try {
-      if (!id && !shiftid && !applicant && !applicant.id) throw new Error(`ERROR INSIDE => ${pathname}. ERROR IS: Your shiftObject (2nd argument) MUST have an id, _shiftID and applicant object!!! Your shiftObject has: ${JSON.stringify(shiftWithApplicantObject)}`);
+      if (!id && !_shiftID && !applicant && !applicant.id) throw new Error(`ERROR INSIDE => ${pathname}. ERROR IS: Your shiftObject (2nd argument) MUST have an id, _shiftID and applicant object!!! Your shiftObject has: ${JSON.stringify(shiftWithApplicantObject)}`);
 
-      const approvedShiftWithApplicant = shiftsWithApplicants.find(shiftWithApplicant => (shiftWithApplicant.id == `${applicant.id}-${shiftid}`));
+      const approvedShiftWithApplicant = shiftsWithApplicants.find(shiftWithApplicant => (shiftWithApplicant.id == `${applicant.id}-${_shiftID}`));
 
       const setShiftStatusesState = (approved_shift_updated) => setShiftStatuses(prv => ({ ...prv, shiftsPendingConfirmation: [...prv.shiftsPendingConfirmation, approved_shift_updated] }));
 
-      const SetAvailableShifts = () => setShiftStatuses(prv => ({ ...prv, shiftsAvailable: shiftsAvailable.filter(({ id }) => id != shiftid) }))
+      const SetAvailableShifts = () => setShiftStatuses(prv => ({ ...prv, shiftsAvailable: shiftsAvailable.filter(({ id }) => id != _shiftID) }))
       
-      const transfer_shift_logic = await TransferApprovedShift(approvedShiftWithApplicant, applicant, shiftid, formattedDateApproved, currentUser, setShiftStatusesState, pathname)
+      const transfer_shift_logic = await TransferApprovedShift(approvedShiftWithApplicant, applicant, _shiftID, formattedDateApproved, currentUser, setShiftStatusesState, pathname)
 
-      const delete_approved_shift = await DeleteApprovedShift(`http://localhost:3003/shiftsAvailable/${shiftid}`, shiftsAvailable, approvedShiftWithApplicant, shiftid, SetAvailableShifts, pathname)
+      const delete_approved_shift = await DeleteApprovedShift(`http://localhost:3003/shiftsAvailable/${_shiftID}`, shiftsAvailable, approvedShiftWithApplicant, _shiftID, SetAvailableShifts, pathname)
 
       const filter_shiftIDfromShiftsWithApplicants = shiftsWithApplicants.filter(shiftWithApplicant => shiftWithApplicant.shiftID == _shiftID);
 
@@ -52,7 +52,9 @@ const ShiftCandidatesPage = () => {
 
       const setStateForShiftsWithApplicants = await FetchDataSetState("http://localhost:3003/shiftsWithApplicants", data => setShiftStatuses(prv => ({ ...prv, shiftsWithApplicants: [...data] }))) //resets shiftWithApplicants state.
       
-      return { transfer_shift_logic, delete_approved_shift, deleteShiftIDsFromShiftsWithApplicants, setStateForShiftsWithApplicants };
+      console.log({ transfer_shift_logic, delete_approved_shift, deleteShiftIDsFromShiftsWithApplicants, setStateForShiftsWithApplicants });
+
+      return
     } catch (error) {
       console.error({ message: "ERROR with onApproveRequest function!!!", location: pathname, error, errorStack: error.stack, errorMessage: error.message, name: error.name });
     }
@@ -61,19 +63,23 @@ const ShiftCandidatesPage = () => {
   return (
     <div>
       <header className='my-5'>
-        <h3>CANDIDATES FOR SHIFT <span className='text-danger'>{JSON.stringify(thisShiftWithApplicants)}</span>:</h3>
+        <h3>CANDIDATES FOR SHIFT <span className='text-danger'>{_shiftID}</span>:</h3>
       </header>
       <Container>
         <div className='all-applicants-div'>
           {
-            applicantsForThisShift.map(({ id, name, base }, idx) => (
+            applicantsForThisShift.map(({ id, applicant }, idx) => (
               <div className={idx % 2 == 0 ? 'candidate-detail p-3 mx-5' : 'candidate-detail-ii p-3 mx-5'} key={idx}>
-                <h1>NAME: <span className='text-danger'>{name}</span></h1>
-                <h3>EMPLOYEE_ID: <span className='text-danger'>{id}</span></h3>
-                <h3>BASE STORE: <span className='text-danger'>{base}</span></h3>
+                <h5>_shiftWithApplicantID: <span className='text-danger'>{id}</span>.</h5>
+                <h1>NAME: <span className='text-danger'>{applicant.name}</span></h1>
+                <h3>EMPLOYEE_ID: <span className='text-danger'>{applicant.id}</span></h3>
+                <h3>BASE STORE: <span className='text-danger'>{applicant.base}</span></h3>
                 <div className='w-100'>
-                  <Button className='w-100' size='md' color={idx % 2 == 1 ? 'success' : 'danger'} onClick={() => onApproveRequest(_shiftID, {})}>
-                    <strong>APPROVE {name}'S REQUEST FOR SHIFT #{_shiftID}!!!</strong>
+                  <Button className='w-100' size='md' color={idx % 2 == 1 ? 'success' : 'danger'} onClick={() => onApproveRequest(_shiftID, { applicant, id })
+                    .then(navigate(`/supervisor/welcome/${currentUser.id}/available shifts`))
+                    .catch(error => console.error({ message: "navigate error!!!", error, errorMessage: error.message }))}
+                  >
+                    <strong>APPROVE {applicant.name}'S REQUEST FOR SHIFT #{_shiftID}!!!</strong>
                   </Button>
                 </div>
               </div>
