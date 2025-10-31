66fb9d78386e4c9fb164902d6ab782dc1aec5416
admin_dho <davidhorph@hotmail.com>
2025-10-30 22:56:36 -0500

    DeleteApprovedShift.
    Function:
    
    1. finds the shift inside shiftsAvailable.
    
    2. After it finds the shift using findShiftInShiftsAvailable, it calls DeleteRequestSetState which does 2 things:
    
    2a: It deletes that shift from shiftsAvailable and then...
    
    2b: the setStateWrapper( ) would reset the state for shiftsAvaialble.

--- src/components/shared/scheduling_needs/functions.jsx
+++ src/components/shared/scheduling_needs/functions.jsx
@@ -22,6 +22,21 @@ export const findShiftInArray = (id_of_shift, array) => {
    return array.filter(({ shiftID }) => shiftID == id_of_shift)
 };
 
+export const DeleteApprovedShift = async (url, shiftsAvailable, approvedShiftWithApplicant, shiftid, setStateWrapper, location) => {
+   try {
+      const findShiftInShiftsAvailable = shiftsAvailable.find(shift => (approvedShiftWithApplicant.shiftID == shift.id)); //find shift to be deleted.
+
+      const DeleteFromShiftsAvaiable = await DeleteRequestSetState(
+         `http://localhost:3003/shiftsAvailable/${shiftid}`, setStateWrapper(), findShiftInShiftsAvailable, location
+      ); //delete findThisShiftInShiftsAvailable and set state.
+
+   } catch (error) {
+      console.error({ message: "deleteApprovedShift ERROR!!!", location, error, errorName: error.name, errorMessage: error.message, traceStack: error.stack, errorCode: error.code });
+
+      return { message: "deleteApprovedShift ERROR!!!", location, error, errorName: error.name, errorMessage: error.message, traceStack: error.stack, errorCode: error.code }
+   }
+}
+
 export const didApplicantApplyForShift = (_currentUserID, shiftID, shiftsWithApplicants) => shiftsWithApplicants.find(({ id }) => (id.includes(_currentUserID)) && (id.includes(shiftID)))
 
 export const youveGotApplicants = (shiftID_toFind, shiftsWithApplicants) => shiftsWithApplicants.filter(shiftWithApplicant => shiftWithApplicant.shiftID == shiftID_toFind)
