c3af0e7ecfa7074f045b9204838f811673f852a8
admin_dho <davidhorph@hotmail.com>
2025-11-03 23:34:56 -0600

    reactoring the body of onApproveRequest.
    All this that I commented out inside the body of onApproveRequest:
    
    // const delete_approved_shift = await DeleteApprovedShift(`http://localhost:3003/shiftsAvailable/${_shiftID}`, shiftsAvailable, approvedShiftWithApplicant, _shiftID, SetAvailableShifts, pathname)
    
          // const filter_shiftIDfromShiftsWithApplicants = shiftsWithApplicants.filter(shiftWithApplicant => shiftWithApplicant.shiftID == _shiftID);
    
          // const deleteShiftIDsFromShiftsWithApplicants = await Promise.all(filter_shiftIDfromShiftsWithApplicants.map(async ({ id }) => await DeleteRequest(`http://localhost:3003/shiftsWithApplicants/${id}`)));
    
          // const setStateForShiftsWithApplicants = await FetchDataSetState("http://localhost:3003/shiftsWithApplicants", data => setShiftStatuses(prv => ({ ...prv, shiftsWithApplicants: [...data] }))) //resets shiftWithApplicants state.
    
          // console.log({ transfer_shift_logic, delete_approved_shift, deleteShiftIDsFromShiftsWithApplicants, setStateForShiftsWithApplicants });
    
    I plan to replace it with more efficient code.
    
    I also commented out this body of code above transfer_shift_logic:
    
          // const shiftIDsPendingConfirmation = shiftStatuses.shiftsPendingConfirmation.map(({ id }) => id);
    
          // const setShiftStatusesState = (approved_shift_updated) => {
          //   const updatedShiftStatus = {...shiftStatuses, shiftsWithApplicants: shiftStatuses.shiftsWithApplicants.filter(shiftWithApplicant => !shiftIDsPendingConfirmation.includes(shiftWithApplicant.id)), shiftsPendingConfirmation: [...shiftStatuses.shiftsPendingConfirmation, approved_shift_updated]}
    
          //   console.log({ approved_shift_updated });
    
          //   return setShiftStatuses(prv => ({
          //     ...prv,
          //     shiftsWithApplicants: prv.shiftsWithApplicants.filter(({ id }) => id != _shiftID), shiftsPendingConfirmation: [...prv.shiftsPendingConfirmation, approved_shift_updated]
          //   }));
          // }
    
    The shiftsNeededConfirmation works (before, it only showed 1 applicant).
    
    transfer_shift_logic is a function I created to delete approved shiftsWithCandidates, delete the approved shift from shiftsAvailable and to POST that shift to shiftsPendingConfirmation.

--- src/pages/supervisor/shift/ShiftCandidatesPage.jsx
+++ src/pages/supervisor/shift/ShiftCandidatesPage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { useLocation, useNavigate, useParams } from 'react-router-dom'
 import { DateTime } from 'luxon';
 
@@ -19,48 +19,60 @@ const ShiftCandidatesPage = () => {
   const { pathname } = useLocation();
   const navigate = useNavigate();
   const { currentUser, setShiftStatuses, shiftStatuses } = useContext(ShiftContext);
+  const { shiftsWithApplicants, shiftsAvailable } = shiftStatuses;
+  
   const { _shiftID } = useParams();
-
-  const { shiftsWithApplicants, shiftsAvailable} = shiftStatuses;
 
   const dateApproved = DateTime.local()
   const formattedDateApproved = dateApproved.toFormat("yyyy-MM-dd");
-
+ 
   const applicantsForThisShift = shiftsWithApplicants.filter(({ shiftID }) => shiftID == _shiftID)
-
   const thisShiftWithApplicants = findShiftInArray(_shiftID, shiftsWithApplicants);
 
   const onApproveRequest = async (_shiftID, shiftWithApplicantObject) => {
-
     try {
       if (!shiftWithApplicantObject.id && !_shiftID && !applicant && !applicant.id) throw new Error(`ERROR INSIDE => ${pathname}. ERROR IS: Your shiftObject (2nd argument) MUST have an id, _shiftID and applicant object!!! Your shiftObject has: ${JSON.stringify(shiftWithApplicantObject)}`);
 
       const { id, applicant } = shiftWithApplicantObject;
 
-      const approvedShiftWithApplicant = shiftsWithApplicants.find(shiftWithApplicant => (shiftWithApplicant.id == {id}));
+      const approvedShiftWithApplicant = shiftsWithApplicants.find(shiftWithApplicant => (shiftWithApplicant.id == id));
 
-      const setShiftStatusesState = (approved_shift_updated) => setShiftStatuses(prv => ({ ...prv, shiftsPendingConfirmation: [...prv.shiftsPendingConfirmation, approved_shift_updated] }));
+      // const shiftIDsPendingConfirmation = shiftStatuses.shiftsPendingConfirmation.map(({ id }) => id);
 
-      const SetAvailableShifts = () => setShiftStatuses(prv => ({ ...prv, shiftsAvailable: shiftsAvailable.filter(({ id }) => id != _shiftID) }))
+      // const setShiftStatusesState = (approved_shift_updated) => {
+      //   const updatedShiftStatus = {...shiftStatuses, shiftsWithApplicants: shiftStatuses.shiftsWithApplicants.filter(shiftWithApplicant => !shiftIDsPendingConfirmation.includes(shiftWithApplicant.id)), shiftsPendingConfirmation: [...shiftStatuses.shiftsPendingConfirmation, approved_shift_updated]}
+
+      //   console.log({ approved_shift_updated });
+
+      //   return setShiftStatuses(prv => ({
+      //     ...prv,
+      //     shiftsWithApplicants: prv.shiftsWithApplicants.filter(({ id }) => id != _shiftID), shiftsPendingConfirmation: [...prv.shiftsPendingConfirmation, approved_shift_updated]
+      //   }));
+      // }
       
-      const transfer_shift_logic = await TransferApprovedShift(approvedShiftWithApplicant, applicant, _shiftID, formattedDateApproved, currentUser, setShiftStatusesState, pathname)
+      const transfer_shift_logic = await TransferApprovedShift(approvedShiftWithApplicant, shiftsWithApplicants, formattedDateApproved, currentUser, pathname)
 
-      const delete_approved_shift = await DeleteApprovedShift(`http://localhost:3003/shiftsAvailable/${_shiftID}`, shiftsAvailable, approvedShiftWithApplicant, _shiftID, SetAvailableShifts, pathname)
+      console.log({ transfer_shift_logic });
 
-      const filter_shiftIDfromShiftsWithApplicants = shiftsWithApplicants.filter(shiftWithApplicant => shiftWithApplicant.shiftID == _shiftID);
+      // const delete_approved_shift = await DeleteApprovedShift(`http://localhost:3003/shiftsAvailable/${_shiftID}`, shiftsAvailable, approvedShiftWithApplicant, _shiftID, SetAvailableShifts, pathname)
 
-      const deleteShiftIDsFromShiftsWithApplicants = await Promise.all(filter_shiftIDfromShiftsWithApplicants.map(async ({ id }) => await DeleteRequest(`http://localhost:3003/shiftsWithApplicants/${id}`)));
+      // const filter_shiftIDfromShiftsWithApplicants = shiftsWithApplicants.filter(shiftWithApplicant => shiftWithApplicant.shiftID == _shiftID);
 
-      const setStateForShiftsWithApplicants = await FetchDataSetState("http://localhost:3003/shiftsWithApplicants", data => setShiftStatuses(prv => ({ ...prv, shiftsWithApplicants: [...data] }))) //resets shiftWithApplicants state.
+      // const deleteShiftIDsFromShiftsWithApplicants = await Promise.all(filter_shiftIDfromShiftsWithApplicants.map(async ({ id }) => await DeleteRequest(`http://localhost:3003/shiftsWithApplicants/${id}`)));
+
+      // const setStateForShiftsWithApplicants = await FetchDataSetState("http://localhost:3003/shiftsWithApplicants", data => setShiftStatuses(prv => ({ ...prv, shiftsWithApplicants: [...data] }))) //resets shiftWithApplicants state.
       
-      console.log({ transfer_shift_logic, delete_approved_shift, deleteShiftIDsFromShiftsWithApplicants, setStateForShiftsWithApplicants });
+      // console.log({ transfer_shift_logic, delete_approved_shift, deleteShiftIDsFromShiftsWithApplicants, setStateForShiftsWithApplicants });
 
-      return
+      return transfer_shift_logic;
     } catch (error) {
       console.error({ message: "ERROR with onApproveRequest function!!!", location: pathname, error, errorStack: error.stack, errorMessage: error.message, name: error.name });
     }
   }
 
+  useEffect(() => {
+    console.log(shiftStatuses);
+  }, [shiftsWithApplicants.length, shiftsAvailable.length])
   return (
     <div>
       <header className='my-5'>
@@ -72,7 +84,12 @@ const ShiftCandidatesPage = () => {
             applicantsForThisShift.map(({ id, applicant }, idx) => (
               <div className={idx % 2 == 0 ? 'candidate-detail p-3 mx-5' : 'candidate-detail-ii p-3 mx-5'} key={idx}>
                 <h5>_shiftWithApplicantID: <span className='text-danger'>{id}</span>.</h5>
-                <h1>NAME: <span className='text-danger'>{applicant.name}</span></h1>
+                {
+                  applicant.name.length > 11 ?
+                    <h3 className='text-danger'>{applicant.name}</h3>
+                    :
+                    <h1 className='text-danger'>{applicant.name}</h1>
+                }
                 <h3>EMPLOYEE_ID: <span className='text-danger'>{applicant.id}</span></h3>
                 <h3>BASE STORE: <span className='text-danger'>{applicant.base}</span></h3>
                 <div className='w-100'>
