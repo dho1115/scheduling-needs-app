b4bb413aed147fcdac275a2e3ef604832bbef0ba
admin_dho <davidhorph@hotmail.com>
2025-11-04 19:51:01 -0600

    Update NavigationBar.jsx

--- src/components/shared/navigation-bar/NavigationBar.jsx
+++ src/components/shared/navigation-bar/NavigationBar.jsx
@@ -27,8 +27,6 @@ const NavigationBar = () => {
     .catch(error => console.error({ message: 'error on handleLogoff function!!!', error, errorCode: error.code, errorStatus: error.status, errorMessage: error.message }));
   
   const [navigationLinks, setNavigationLinks] = useState(NavigationLinks({ id, role }));
-
-  console.log({ shiftsAvailable, shiftsWithApplicants });
   
   useEffect(() => setNavigationLinks(NavigationLinks({ id, role }, shiftsWithApplicants.length ? `candidates` : '')), [navigationLinks.length, id, role, shiftsAvailable.length, shiftsWithApplicants.length]);
 
