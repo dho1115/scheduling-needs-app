55d0928ed0d0d01e45fd96ed3629c934bccffb09
admin_dho <davidhorph@hotmail.com>
2025-11-05 12:39:25 -0600

    Deleted.

--- src/functions/putRequest.js
+++
@@ -1,7 +0,0 @@
-export const PutRequest = (url, id, updatedData) => fetch(url, {
-   method: 'PUT',
-   headers: {
-      'Content-Type': 'application/json; charset=UTF-8'
-   },
-   body: JSON.stringify(updatedData)
-})
