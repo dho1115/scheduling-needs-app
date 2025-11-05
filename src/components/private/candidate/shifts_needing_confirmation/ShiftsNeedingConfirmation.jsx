9ea735590499b61a62b35f2832e813f80841cd63
admin_dho <davidhorph@hotmail.com>
2025-11-04 19:50:56 -0600

    created the shift objects to be confirmed
    Created shift_object that includes details about the shift, including dateConfirmed, which will contain the data/properties to be transferred to shiftsConfirmed.

--- src/components/private/candidate/shifts_needing_confirmation/ShiftsNeedingConfirmation.jsx
+++ src/components/private/candidate/shifts_needing_confirmation/ShiftsNeedingConfirmation.jsx
@@ -1,23 +1,32 @@
 import React, { Suspense, useContext } from 'react';
+import { useLocation } from 'react-router-dom';
 import { useNavigate, useParams } from 'react-router-dom';
 import { ShiftContext } from '../../../../App'
 import { Button, Container } from 'reactstrap';
 import { DateTime } from 'luxon';
+import { TransferFromPendingConfirmation } from './functions';
 import LoadingComponent from '../../../LoadingComponent';
 
 import "./ShiftsNeedingConfirmation.styles.css";
 
 const ShiftsNeedingConfirmation = () => {
+   const { pathname } = useLocation();
    const { id } = useParams();
    const navigate = useNavigate();
    const { shiftStatuses: {shiftsPendingConfirmation}, setShiftStatuses  } = useContext(ShiftContext);
 
    const handleShiftConfirmation = async val => {
-      const dateConfirmed = DateTime.now().toFormat('yyyy-MM-dd');
-      const confirmationDetails = {...val, dateConfirmed };
-      console.log(confirmationDetails);
+      try {
+         const dateConfirmed = DateTime.now().toFormat('yyyy-MM-dd');
+         const shift_object = { ...val, dateConfirmed };
+         const TransferShift = TransferFromPendingConfirmation(`http://localhost:3003/shiftsPendingConfirmation/${val.shiftWithApplicantID}`, shift_object, pathname)
 
-      alert(`${val.shiftID} (store number ${val.storeNumber}) has just been confirmed by ${val.applicantName} on ${dateConfirmed}!!!`);
+         return { TransferShift, val, shift_object, urlDeleted: `http://localhost:3003/shiftsPendingConfirmation/${val.shiftWithApplicantID}`};
+      } catch (error) {
+         console.error({ message: "ERROR from handleShiftConfirmation!!!", error, errorMessage: error.message, errorStack: error.stack, errorType: error.type, val, shiftNeedingConfirmation_id: val.shiftWithApplicantID});
+
+         return { message: "ERROR from handleShiftConfirmation!!!", error, errorMessage: error.message, errorStack: error.stack, errorType: error.type, val, shiftWithApplicantID: val.id }
+      }      
    }
 
    return (
@@ -27,7 +36,7 @@ const ShiftsNeedingConfirmation = () => {
                const { id, shiftID, date, time, storeNumber, approvedOn, applicant: { id: applicantID, name, role } } = val;
                
                return (
-                  <div key={idx} className='m-1 p-1' style={{ overflowWrap: 'break-word', overflow: 'hidden', border: '5px solid firebrick', backgroundColor: 'bisque' }}>
+                  <div key={idx} className='m-1 p-1' style={{ overflowWrap: 'break-word', overflow: 'hidden', border: '5px solid firebrick', backgroundColor: 'lightgreen' }}>
                      <Suspense fallback={<LoadingComponent />}>
                         <h5>shiftWithApplicant ID: <span className='text-danger'>{id}</span> was approved on <span className='text-danger'>{approvedOn}</span>.</h5>
                         <h5>ID for this shift: <span className='text-danger'>{shiftID}</span>.</h5>
@@ -37,7 +46,7 @@ const ShiftsNeedingConfirmation = () => {
                         <hr />
                         <h5>APPLICANT ASSIGNED TO WORK SHIFT # {shiftID}:</h5>
                         <h5>Applicant Name/applicant id: {name} - {applicantID}.</h5>
-                        <Button color='danger' size='xl' className='w-100' onClick={() => handleShiftConfirmation({ shiftWithApplicantID: id, shiftID, applicantName: name, applicantID, date_of_shift: date, storeNumber, time })}>PLEASE CONFIRM THIS DAMN SHIFT!!!</Button>
+                        <Button color='danger' size='xl' className='w-100' onClick={() => handleShiftConfirmation({id, shiftWithApplicantID: id, shiftID, applicantName: name, applicantID, date_of_shift: date, storeNumber, time })}>PLEASE CONFIRM THIS DAMN SHIFT!!!</Button>
                      </Suspense>
                   </div>
                )
