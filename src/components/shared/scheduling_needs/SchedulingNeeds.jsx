5d579210eb3e84fc5fc6fb1e62da0a76f55973e4
admin_dho <davidhorph@hotmail.com>
2025-11-05 19:03:54 -0600

    Added some checks for shiftStatus.

--- src/components/shared/scheduling_needs/SchedulingNeeds.jsx
+++ src/components/shared/scheduling_needs/SchedulingNeeds.jsx
@@ -1,5 +1,5 @@
 //Dependencies.
-import { useContext } from 'react'
+import { Suspense, useContext, useEffect } from 'react'
 import { ShiftContext } from '../../../App';
 import Shift from './shift_component/Shift';
 import { Container } from 'reactstrap';
@@ -8,7 +8,11 @@ import ErrorBoundary from '../../ErrorBoundary';
 import "./SchedulingNeeds.styles.css";
 
 const SchedulingNeeds = () => {
-   const { shiftStatuses: { shiftsAvailable } } = useContext(ShiftContext);
+   const { shiftStatuses, shiftStatuses: { shiftsAvailable } } = useContext(ShiftContext);
+
+   useEffect(() => {
+      if (!shiftsAvailable || !shiftsAvailable.length) console.error("*** WARNING!!! *** shiftsAvailable returned: ", shiftsAvailable);
+   }, [])
    
    return (
       <div>
@@ -16,13 +20,17 @@ const SchedulingNeeds = () => {
             <h1>SCHEDULING NEEDS.</h1>
          </header>
          <Container className='p-3 scheduling-needs-container'>
-            {
-               shiftsAvailable.map((val, idx) => (
-                  <ErrorBoundary fallback={<h3 className='text-danger'>ERROR!!!</h3>}>
-                     <Shift key={idx} {...val} idx={idx} />
-                  </ErrorBoundary>)
-               )
-            }
+            <Suspense fallback={<h5>Loading shiftsAvailable...</h5>}>
+               {
+                  shiftsAvailable.map((val, idx) => ( /* shiftsAvailable is not a function??? */
+                     <ErrorBoundary fallback={<h3 className='text-danger'>ERROR!!!</h3>}>
+                        <Suspense fallback={<h3 className='text-danger'>LOADING...</h3>}>
+                           <Shift key={idx} {...val} idx={idx} />
+                        </Suspense>
+                     </ErrorBoundary>)
+                  )
+               }
+            </Suspense>
          </Container>
       </div>
    )
