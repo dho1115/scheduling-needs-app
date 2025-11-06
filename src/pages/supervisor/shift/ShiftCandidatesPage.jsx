ab96683c151364371d3ec2ef62503444a3cbdcb8
admin_dho <davidhorph@hotmail.com>
2025-11-05 19:09:23 -0600

    Added emailjs keys, error validation. ALSO...
    Called the function to send email to emailjs:
    
    ConfirmApprovedShiftLogic(_shiftID, date, "http://localhost:3001/", applicant.name, storeNumber, emailjs, emailjs_keys.SERVICE_ID, emailjs_keys.CONFIRM_SHIFT_KEY_ID, emailjs_keys.PUBLIC_KEY_ID)
    
    This is inside the onApprovedShift inside the .then( ).
    
    IMPORTANT!!!
    
    I made the following correction in the <Button></Button>:
    
    .then(( ) => navigate(`/supervisor/welcome/${currentUser.id}/available shifts`))

--- src/pages/supervisor/shift/ShiftCandidatesPage.jsx
+++ src/pages/supervisor/shift/ShiftCandidatesPage.jsx
@@ -5,8 +5,9 @@ import { DateTime } from 'luxon';
 //components.
 import { Button, Container } from 'reactstrap';
 
-//context.
-import { ShiftContext } from '../../../App'
+//context and dependencies.
+import { ShiftContext } from '../../../App';
+import emailjs from '@emailjs/browser';
 
 //functions.
 import { FetchShiftStatuses } from '../../../functions/FetchHook';
@@ -18,7 +19,7 @@ import "./ShiftCandidatesPage.styles.css"
 const ShiftCandidatesPage = () => {
   const { pathname } = useLocation();
   const navigate = useNavigate();
-  const { currentUser, setShiftStatuses, shiftStatuses } = useContext(ShiftContext);
+  const { currentUser, setShiftStatuses, shiftStatuses, emailjs_keys } = useContext(ShiftContext);
   const { shiftsWithApplicants, shiftsAvailable } = shiftStatuses;
   
   const { _shiftID } = useParams();
@@ -30,18 +31,44 @@ const ShiftCandidatesPage = () => {
 
   const onApproveRequest = async (_shiftID, shiftWithApplicantObject) => {
     try {
-      if (!shiftWithApplicantObject.id && !_shiftID && !applicant && !applicant.id) {
-        throw new Error(`ERROR INSIDE => ${pathname}. Your shiftObject (2nd argument) MUST have an id, _shiftID and applicant object!!! Your shiftObject has: ${JSON.stringify(shiftWithApplicantObject)}`);
+      if (!shiftWithApplicantObject.id && !_shiftID && !applicant && !applicant.id && !shiftWithApplicantObject.date && !shiftWithApplicantObject.storeNumber) {
+        throw new Error(`ERROR INSIDE => ${pathname}. Your shiftObject (2nd argument) MUST have an id, _shiftID, a shiftDate, a storeNumber and applicant object!!! Your shiftObject has: ${JSON.stringify(shiftWithApplicantObject)}`);
       }
 
-      const { id, applicant } = shiftWithApplicantObject;
+      const { id, applicant, date, storeNumber } = shiftWithApplicantObject;
 
       const approvedShiftWithApplicant = shiftsWithApplicants.find(shiftWithApplicant => (shiftWithApplicant.id == id));
       
       const transfer_shift_logic = await TransferApprovedShift(approvedShiftWithApplicant, shiftsWithApplicants, formattedDateApproved, currentUser, pathname)
 
       FetchShiftStatuses()
-        .then(result => setShiftStatuses(result))
+        .then(arrayOfResponses => {
+          const allResponsesOK = arrayOfResponses.filter(response => !response.ok);
+
+          if (!allResponsesOK.length) {
+            const jsonifyResponses = arrayOfResponses.reduce((accumulator, res) => {
+              accumulator = { ...accumulator, [res]: { ...res.json() } };
+              return accumulator;
+            }, {});
+
+            return jsonifyResponses
+          }
+          else {
+            throw new Error(`Please wait while we resolve all responses. At least one returned !res.ok: Currently, the followning responses are NOT ok: ${JSON.stringify(allResponsesOK)}.`)
+          }
+        })
+        .then(async shiftStatuses => {
+          console.log("returning shiftStatuses...", shiftStatuses);
+          setShiftStatuses(shiftStatuses);
+
+          return await ConfirmApprovedShiftLogic(_shiftID, date, "http://localhost:3001/", applicant.name, storeNumber, emailjs, emailjs_keys.SERVICE_ID, emailjs_keys.CONFIRM_SHIFT_KEY_ID, emailjs_keys.PUBLIC_KEY_ID)
+        })
+        .then(response => {
+          if (response.status == 200 || response.text) {
+            console.log("Message send successfully!!!")
+            console.log(response.templateParams);
+          }
+        })
         .catch(error => ({ message: "FetchShiftStatuses function call ERROR!!!", location: location.pathname, error, errorName: error.name, errorMessage: error.message, errorStack: error.stack }));
 
       console.log({ transfer_shift_logic });
@@ -63,7 +90,7 @@ const ShiftCandidatesPage = () => {
       <Container>
         <div className='all-applicants-div'>
           {
-            applicantsForThisShift.map(({ id, applicant }, idx) => (
+            applicantsForThisShift.map(({ id, applicant, date, storeNumber }, idx) => (
               <div className={idx % 2 == 0 ? 'candidate-detail p-3 mx-5' : 'candidate-detail-ii p-3 mx-5'} key={idx}>
                 <h5>_shiftWithApplicantID: <span className='text-danger'>{id}</span>.</h5>
                 {
@@ -74,9 +101,10 @@ const ShiftCandidatesPage = () => {
                 }
                 <h3>EMPLOYEE_ID: <span className='text-danger'>{applicant.id}</span></h3>
                 <h3>BASE STORE: <span className='text-danger'>{applicant.base}</span></h3>
+                <h3>SHIFT DATE: <span className='text-danger'>{date}</span></h3>
                 <div className='w-100'>
-                  <Button className='w-100' size='md' color={idx % 2 == 1 ? 'success' : 'danger'} onClick={() => onApproveRequest(_shiftID, { applicant, id })
-                    .then(navigate(`/supervisor/welcome/${currentUser.id}/available shifts`))
+                  <Button className='w-100' size='md' color={idx % 2 == 1 ? 'success' : 'danger'} onClick={() => onApproveRequest(_shiftID, { applicant, id, date, storeNumber })
+                    .then(() => navigate(`/supervisor/welcome/${currentUser.id}/available shifts`))
                     .catch(error => console.error({ message: "navigate error!!!", error, errorMessage: error.message }))}
                   >
                     <strong>APPROVE {applicant.name}'S REQUEST FOR SHIFT #{_shiftID}!!!</strong>
