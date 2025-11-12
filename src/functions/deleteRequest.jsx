29e621b624baf68d1f5b7798bc424531e8a1271f
admin_dho <davidhorph@hotmail.com>
2025-11-12 00:26:29 -0600

    Created BatchCleanup function.
    Function to clean up and delete expired shifts from various shiftStatuses properties:
    
    export const BatchCleanup = (DateTime) => {
       const BASE = 'http://localhost:3003/'
       const ENDPOINTS = ['shiftsAvailable', 'shiftsWithApplicants', 'shiftsPendingConfirmation', 'shiftsConfirmed'];
    
       const BATCH_DELETE_FUNCTION = () => ENDPOINTS
          .map(async endpoint => {
             const CURRENT_DATE = DateTime.local();
             const FETCH_DATA = await fetchDataPromise(BASE + endpoint); //[{data}]
             const EXPIRED_IDS = FETCH_DATA
                .filter(({ date }) => DateTime.fromISO(date) < CURRENT_DATE).map(({ id }) => id); //[id]
    
             return EXPIRED_IDS.map(id => DeleteRequest(`${BASE}${endpoint}/${id}`));
          })
    
       const BATCH_DELETE_SHIFTS_CONFIRMED = () => ["shiftsConfirmed"].map(
          async endpoint => {
          const CURRENT_DATE = DateTime.local();
          const FETCH_DATA = await fetchDataPromise(BASE + endpoint) //[{data}].
    
          return FETCH_DATA
             .filter(({ date_of_shift }) => DateTime.fromISO(date_of_shift) < CURRENT_DATE)
             .map(({ id }) => id)
             .map(id => DeleteRequest(`${BASE}${endpoint}/${id}`));
       })
    
       return Promise.all([
          ...BATCH_DELETE_FUNCTION(),
          ...BATCH_DELETE_SHIFTS_CONFIRMED()
       ])
          .then(response => console.log({ message: "Promse.all BATCH DELETE SUCCESS!!!", response }))
          .catch(error => console.error({ message: "Promise.all BATCH_DELETE_FUNCTION Error!!!", error, errorMessage: error.message }));
    }

--- src/functions/deleteRequest.jsx
+++ src/functions/deleteRequest.jsx
@@ -1,3 +1,5 @@
+import { fetchDataPromise } from "./FetchHook";
+
 export const DeleteRequest = (url, id = null) => {
    return fetch(url, { method: 'DELETE' })
       .then(() => {
@@ -7,18 +9,37 @@ export const DeleteRequest = (url, id = null) => {
       .catch(error => console.error({ message: "DeleteRequest error (deleteRequest.jsx).", error, errorMessage: error.message, errorName: error.name }));
 }
 
-export const DeleteBatch = (...args_to_delete) => {
-   try {
-      const convertArrayToItems = Array.from(args_to_delete).forEach(item => {
-         if (typeof (item) == 'object' && item.length) throw new Error(`${JSON.stringify(item)} is an array!!! please enter all arrays as spread operators!!!`)
+export const BatchCleanup = (DateTime) => {
+   const BASE = 'http://localhost:3003/'
+   const ENDPOINTS = ['shiftsAvailable', 'shiftsWithApplicants', 'shiftsPendingConfirmation', 'shiftsConfirmed'];
+
+   const BATCH_DELETE_FUNCTION = () => ENDPOINTS
+      .map(async endpoint => {
+         const CURRENT_DATE = DateTime.local();
+         const FETCH_DATA = await fetchDataPromise(BASE + endpoint); //[{data}]
+         const EXPIRED_IDS = FETCH_DATA
+            .filter(({ date }) => DateTime.fromISO(date) < CURRENT_DATE).map(({ id }) => id); //[id]
          
-         return Promise.all(Array.from(args_to_delete).map(async item => await DeleteRequest(item)));
-      });
-   } catch (error) {
-      console.error({ message: "ERROR DELETING BATCH!!!", error, errorMessage: error.message, errorStack: error.stack, errorCode: error.code });
+         return EXPIRED_IDS.map(id => DeleteRequest(`${BASE}${endpoint}/${id}`));
+      })
+   
+   const BATCH_DELETE_SHIFTS_CONFIRMED = () => ["shiftsConfirmed"].map(
+      async endpoint => {
+      const CURRENT_DATE = DateTime.local();
+      const FETCH_DATA = await fetchDataPromise(BASE + endpoint) //[{data}].
       
-      return error;
-   }
+      return FETCH_DATA
+         .filter(({ date_of_shift }) => DateTime.fromISO(date_of_shift) < CURRENT_DATE)
+         .map(({ id }) => id)
+         .map(id => DeleteRequest(`${BASE}${endpoint}/${id}`));
+   })
+   
+   return Promise.all([
+      ...BATCH_DELETE_FUNCTION(),
+      ...BATCH_DELETE_SHIFTS_CONFIRMED()
+   ])
+      .then(response => console.log({ message: "Promse.all BATCH DELETE SUCCESS!!!", response }))
+      .catch(error => console.error({ message: "Promise.all BATCH_DELETE_FUNCTION Error!!!", error, errorMessage: error.message }));
 }
 
 export const DeleteRequestSetState = async (url, setStateWrapper, data, location=null) => {
