d5eb3ef45e166eac7c6ae9925091495af2afbed4
admin_dho <davidhorph@hotmail.com>
2025-11-19 09:04:20 -0600

    Update patchRequest.jsx

--- src/functions/patchRequest.jsx
+++ src/functions/patchRequest.jsx
@@ -1,4 +1,4 @@
-export const PatchRequest = (url, newData) => fetch(url, {
+export const PatchRequest = async (url, newData) => await fetch(url, {
    method: 'PATCH',
    headers: {
       'Content-Type': 'application/json; charset=UTF-8',
