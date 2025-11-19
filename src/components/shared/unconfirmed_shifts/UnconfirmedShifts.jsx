96365ed1b33777b5af4c97ce0aa4e35408a1cf08
admin_dho <davidhorph@hotmail.com>
2025-11-19 09:03:54 -0600

    Update UnconfirmedShifts.jsx
    Added a new feature. Any work shift that is due is placed above the other shifts with the note, The Following Shift(s) have not been confirmed YET!!!

--- src/components/shared/unconfirmed_shifts/UnconfirmedShifts.jsx
+++ src/components/shared/unconfirmed_shifts/UnconfirmedShifts.jsx
@@ -3,39 +3,60 @@ import React, { useContext } from 'react'
 
 //Dependencies.
 import { ShiftContext } from '../../../App';
+import { DateTime } from 'luxon';
 
 import './UnconfirmedShifts.styles.css';
-import { Button } from 'reactstrap';
+import { Button, Container } from 'reactstrap';
 
 import "./UnconfirmedShifts.styles.css";
 
 const UnconfirmedShifts = () => {
-  const { currentUser, shiftStatuses: {shiftsPendingConfirmation} } = useContext(ShiftContext);
+  const { currentUser, shiftStatuses: { shiftsPendingConfirmation } } = useContext(ShiftContext);
+  
+  const upcomingWorkShifts = shiftsPendingConfirmation.filter(({date}) => {
+    const currentDate = DateTime.now();
+    const dateOfShift = DateTime.fromISO(date);
 
-  const handleUnconfirmedShifts = () => {
-    console.log("LOGIC TO CHANGE unconfirmedShifts STATUS.")
-    console.log("setUnconfirmedShifts( logic goes here. )")
-  }
+    return dateOfShift.diff(currentDate, ['days']).values.days < 3.75
+  })
 
   return (
-    <div>
+    <div className='py-5'>
       <header>
         <h1>UNCONFIRMED SHIFTS</h1>
       </header>
+      <div>
+        <h3>The Following Shift(s) With <u>Upcoming Work Dates</u>...</h3>
+        <h3 className='text-danger'><i>HAVE NOT BEEN CONFIRMED YET!!!</i></h3>
+        <Container style={{display: 'grid', gridTemplateColumns: '31% 31% 31%'}}>
+          {
+            upcomingWorkShifts.map((val, idx) => (
+              <div className='m-3 p-1' style={{ border: '3px solid black', overflowWrap: 'anywhere', overflow: 'hidden', backgroundColor: idx%2==1 ? 'whitesmoke':'lightpink' }}>
+                <h5>{JSON.stringify(val)}</h5>
+              </div>
+            ))
+          }
+        </Container>
+      </div>
       {
         shiftsPendingConfirmation.length ?
-          shiftsPendingConfirmation.map((val, idx) => {
-            return (
-              <div key={idx} className={idx%2==0 ? "unconfirmed-shift-div m-3 p-1" : "unconfirmed-shift-div_v2 m-3 p-1"}>
-                <h5><span>_shiftID:</span><span className='text-danger'>{val.id}</span></h5>
-                <h5><span>store number:</span><span className='text-danger'>{val.storeNumber}</span></h5>
-                <h5><span>Awaiting confirmation from:</span><span className='text-danger'>{JSON.stringify(val.applicant)}</span></h5>
-                {
-                  currentUser.role == "candidate" && (<Button color='success' size='md' onClick={handleUnconfirmedShifts}>CONFIRM THIS SHIFT!!!</Button>)
-                }
-              </div>
-            )
-          })
+          <Container className='to-be-confirmed-container'>
+            {
+              shiftsPendingConfirmation.map((val, idx) => {
+                return (
+                  <div key={idx} className={idx%2==0 ? "unconfirmed-shift-div my-1 p-1" : "unconfirmed-shift-div_v2 my-1 p-1"}>
+                    <h5><span>_shiftID:</span><span className='text-danger'>{val.id}</span></h5>
+                    <h5><span>SCHEDULED TO WORK ON: </span><span className='text-danger'>{val.date}</span></h5>
+                    <h5><span>store number:</span><span className='text-danger'>{val.storeNumber}</span></h5>
+                    <h5><span>Awaiting confirmation from:</span><span className='text-danger'>{val.applicant.name} ({val.applicant.id}).</span></h5>
+                    {
+                      currentUser.role == "supervisor" && (<Button color='danger' size='md' onClick={() => alert(`message sent to ${val.applicant.name} to confirm shift# ${val.id} (store# ${val.storeNumber}) in which ${val.applicant.name} is scheduled to work on ${val.date}!!!`)}>SEND REMINDER TO {val.applicant.name.toUpperCase()} TO CONFIRM DAMN SHIFT!!!</Button>)
+                    }
+                  </div>
+                )
+              })
+            }            
+          </Container>          
           :
           <h1 className='text-danger'>YOU DO NOT HAVE ANY UNCONFIRMED SHIFTS.</h1>
       }
