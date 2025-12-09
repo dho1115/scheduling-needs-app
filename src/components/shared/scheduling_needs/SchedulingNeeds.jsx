33dd4672c09f9fcf33650a3f4a5e94df7c15769d
admin_dho <davidhorph@hotmail.com>
2025-12-09 16:00:57 -0600

    New Function.
    handleSendSchedulingNeedsNotification - Sends notification of new shifts.

--- src/components/shared/scheduling_needs/SchedulingNeeds.jsx
+++ src/components/shared/scheduling_needs/SchedulingNeeds.jsx
@@ -1,24 +1,66 @@
 //Dependencies.
+import emailjs from '@emailjs/browser';
 import { Suspense, useContext, useEffect } from 'react'
 import { ShiftContext } from '../../../App';
 import Shift from './shift_component/Shift';
-import { Container } from 'reactstrap';
+import { Button, Container } from 'reactstrap';
 import ErrorBoundary from '../../ErrorBoundary';
+
+//Functions.
+import { DateTime } from 'luxon';
+import { fetchDataPromise } from '../../../functions/FetchHook';
+import { PostRequest } from '../../../functions/postRequest';
+import { PatchRequest } from '../../../functions/patchRequest';
 
 import "./SchedulingNeeds.styles.css";
 
 const SchedulingNeeds = () => {
-   const { shiftStatuses, shiftStatuses: { shiftsAvailable } } = useContext(ShiftContext);
+   const { currentUser, emailjs_keys, shiftStatuses, shiftStatuses: { shiftsAvailable } } = useContext(ShiftContext);
+
+   const { SERVICE_ID, PUBLIC_KEY_ID, GENERAL_KEY_ID, CONFIRM_SHIFT_KEY_ID } = emailjs_keys;
 
    useEffect(() => {
       if (!shiftsAvailable || !shiftsAvailable.length) console.error("*** WARNING!!! *** shiftsAvailable returned: ", shiftsAvailable);
    }, [])
+
+   const handleSendSchedulingNeedsNotification = async (...args) => {
+      alert("Notification has been sent!!!")
+      console.log("NOTIFICATION HAS BEEN SENT!!!")
+      const currentDateTime = DateTime.now();
+      const currentDateTimeISO = DateTime.now().toISO();
+
+      try {
+         const fetchDate = await fetchDataPromise("http://localhost:3003/shiftsNotificationSent");
+
+         if (fetchDate.date == "") {
+            const initializeDateTime = await PatchRequest("http://localhost:3003/shiftsNotificationSent", { date: currentDateTimeISO });
+         }
+
+         const templateParams = { email: import.meta.env.VITE_EMAILJS_DEFAULT_EMAIL, comments: "SCHEULING NEEDS UPDATED!!! (SHIFTS HAVE BEEN ADDED/REMOVED)!!!", _shiftID:"N/A", storeNumber:"", shiftDate: currentDateTime, url:"ttp://localhost:3001/", username:"ALL CANDIDATES!!!" /* candidate name. */ };
+
+         const sendEmail = await emailjs.send(SERVICE_ID, CONFIRM_SHIFT_KEY_ID, templateParams);
+
+         const PatchNotificationSentDate = await PatchRequest("http://localhost:3003/shiftsNotificationSent", { date: currentDateTimeISO });
+
+      } catch (error) {
+         console.error({ message: "handleSendSchedulingNeedsNotification ERROR!!!", error, errorMessage: error.message, errorName: error.name });
+      }
+   } 
    
    return (
       <div>
          <header>
             <h1>SCHEDULING NEEDS.</h1>
          </header>
+         
+         {
+            currentUser.role == 'supervisor'
+            &&
+            <div className='m-3 p-1' style={{float: "right"}}>
+               <Button size='lg' color='danger' onClick={handleSendSchedulingNeedsNotification}>SEND SCHEDULING NEEDS NOTIFICATION!!!</Button>
+            </div>
+         }
+         
          <Container className='p-3 scheduling-needs-container'>
             <Suspense fallback={<h5>Loading shiftsAvailable...</h5>}>
                {
