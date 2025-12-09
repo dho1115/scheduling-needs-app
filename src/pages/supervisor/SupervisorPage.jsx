2debd4d3476720ef311e46b0f17a749db28c606c
admin_dho <davidhorph@hotmail.com>
2025-12-09 16:36:40 -0600

    Created SupervisorPageContext.
    For now, it holds two values: newShfitAdded and setNewShiftAdded.

--- src/pages/supervisor/SupervisorPage.jsx
+++ src/pages/supervisor/SupervisorPage.jsx
@@ -1,25 +1,31 @@
-import { Suspense, useContext } from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { createContext, Suspense, useContext, useState } from 'react';
+import { Outlet } from 'react-router-dom';
 import NavigationBar from '../../components/shared/navigation-bar/NavigationBar';
 import { ShiftContext } from '../../App';
 import { Container } from 'reactstrap';
 
 import "./SupervisorPage.styles.css";
 
+export const SupervisorPageContext = createContext();
+
 const SupervisorPage = () => {
   const { currentUser } = useContext(ShiftContext);
+  const [newShiftAdded, setNewShiftAdded] = useState(false);
   
   return (
     <div>
-      <Suspense fallback={<h1>Loading data... please wait while we load {JSON.stringify(currentUser)}.</h1>}>
-        <NavigationBar />
-        <div className='p-1'>
-          <h3>Welcome to your page, <span style={{color: 'red'}}>{currentUser.name}</span>!!!</h3>
-        </div>
-        <Container>
-          <Outlet />
-        </Container>
-      </Suspense>      
+      <SupervisorPageContext.Provider value={{newShiftAdded, setNewShiftAdded}}>
+        <Suspense fallback={<h1>Loading data... please wait while we load {JSON.stringify(currentUser)}.</h1>}>
+          <NavigationBar />
+          <div className='p-1'>
+            <h3>Welcome to your page, <span style={{color: 'red'}}>{currentUser.name}</span>!!!</h3>
+          </div>
+          <Container>
+            <Outlet />
+          </Container>
+        </Suspense>
+      </SupervisorPageContext.Provider>
+            
     </div>
   )
 }
