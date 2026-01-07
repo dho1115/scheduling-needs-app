91833803e9162510b6ea0aac0a8e39b90a833e4b
admin_dho <davidhorph@hotmail.com>
2026-01-07 11:16:57 -0600

    changed the name of the function.

--- src/functions/firebase/authorization.jsx
+++ src/functions/firebase/authorization.jsx
@@ -1,7 +1,7 @@
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { auth } from "../../firebase";
 
-export const signUpNewUser = async (email, password, location=null) => {
+export const fb_signUpNewUser = async (email, password, location=null) => {
    try {
       const currentUser_credential = await createUserWithEmailAndPassword(auth, email, password);
       const userCredential = currentUser_credential.user;
@@ -11,14 +11,25 @@ export const signUpNewUser = async (email, password, location=null) => {
    }
 }
 
-export const userLogin = async (email, password, location=null) => {
+export const fb_userLogin = async (email, password, location=null) => {
    try {
       const login_credential = await signInWithEmailAndPassword(auth, email, password);
       const currentUser = login_credential.user;
       console.log(`Successfully logged in ${JSON.stringify(currentUser)}.`);
-      return currentUser;      
+      return currentUser;  
    } catch (error) {
       console.error({ message: "userLogin error (authorization.jsx)", location, error, errorMessage: error.message, errorName: error.name });
+   }
+}
+
+export const fb_signOut = async () => {
+   try {
+      const sign_out = await signOut(auth);
+      console.log({ message: "fb_signOut SUCCESSFUL!!!", authCurrentUser: auth.currentUser, sign_out });
+      
+      return { message: "fb_signOut SUCCESSFUL!!!", authCurrentUser: auth.currentUser, sign_out };
+   } catch (error) {
+      console.error({message: "ERROR in fb_signOut.", error, errorMessage: error.message, errorName: error.name})
    }
 }
 
