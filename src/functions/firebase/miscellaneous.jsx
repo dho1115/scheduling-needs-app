63870bca0917e682efd3b7ad09145f711ce94a38
admin_dho <davidhorph@hotmail.com>
2026-01-22 20:23:41 -0600

    Changed the logic in fb_fs_SignOutProcess.
    The logic now signs out of Firebase and has a setCurrentUserDeclaration parameter that will take in ( ) => setCurrentUser( { ... } ) back to initial state when the user signs out.

--- src/functions/firebase/miscellaneous.jsx
+++ src/functions/firebase/miscellaneous.jsx
@@ -1,5 +1,5 @@
-import { fb_signUpNewUser, fb_userLogin } from "./authorization";
-import { fb_addOneDocument } from "./crud_basic";
+import { fb_signOut, fb_signUpNewUser, fb_userLogin } from "./authorization";
+import { fb_addOneDocument, fb_deleteOneDocument } from "./crud_basic";
 
 export const fb_fs_NewUserRegistration = async (signUp_details, location=null) => {
    const fake_email = `${signUp_details.name.split(" ").join("") + signUp_details.id}@email.com`
@@ -18,12 +18,32 @@ export const fb_fs_ExistingUserLogin = async (signUp_details, location) => {
    const fake_email = `${signUp_details.name.split(" ").join("") + signUp_details.id}@email.com`;
    try {
       return await Promise.all([
-         fb_userLogin(fake_email, signUp_details.password, location), fb_addOneDocument('Current User', signUp_details, signUp_details.id, location),
-         fb_addOneDocument('Employees', signUp_details, location)
+         await fb_userLogin(fake_email, signUp_details.password, location),
+         await fb_addOneDocument('Current User', signUp_details, signUp_details.id, location)
       ]);
    } catch (error) {
       console.error({ message: "ERROR inside fb_fs_ExistingUserLogin (miscellaneous.jsx)!!!", location, error, fake_email, errorStack: error.stack, errorName: error.name, errorCode: error.code, errorMessage: error.message })
       
-      return {message: "ERROR inside fb_fs_ExistingUserLogin (miscellaneous.jsx)!!!", location, error, fake_email, errorStack: error.stack, errorName: error.name, errorCode: error.code, errorMessage: error.message}
+      return { message: "ERROR inside fb_fs_ExistingUserLogin (miscellaneous.jsx)!!!", location, error, fake_email, errorStack: error.stack, errorName: error.name, errorCode: error.code, errorMessage: error.message }
+   }
+}
+
+export const fb_fs_SignOutProcess = async (_currentUserID, setCurrentUserDeclaration, currentUser, location) => {
+   try {
+      const set_current_user = () => new Promise((res, rej) => {
+         console.log("currentUser was", currentUser)
+         setCurrentUserDeclaration();
+         res({ message: "Successully set currentUser!!!", currentUser });
+         rej({ error_message: `currentUser is still ${JSON.stringify(currentUser)}!!!` })
+      });
+
+      return await Promise.all([
+         fb_signOut(_currentUserID, location),
+         fb_deleteOneDocument("Current User", _currentUserID), set_current_user()
+            .then(result => console.log(result))
+            .catch(error => console.error(error))
+      ])
+   } catch (error) {
+      console.error({ message: "Error in fb_fs_SignOutProcess (miscellaneous.jsx)", location, error, errorMessage: error.message, errorStack: error.stack, errorName: error.name });
    }
 }
