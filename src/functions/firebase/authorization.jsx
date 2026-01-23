71f359381abc41eda9c51d3143b6761e11c2bef6
admin_dho <davidhorph@hotmail.com>
2026-01-22 20:17:57 -0600

    Update authorization.jsx
    instead of returning currentUser, it now returns login_credential which is the result of await signInWithEmailAndPassword.

--- src/functions/firebase/authorization.jsx
+++ src/functions/firebase/authorization.jsx
@@ -1,4 +1,5 @@
 import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { fb_deleteOneDocument } from "./crud_basic";
 import { auth } from "../../firebase";
 
 export const fb_signUpNewUser = async (email, password, location=null) => {
@@ -11,7 +12,8 @@ export const fb_signUpNewUser = async (email, password, location=null) => {
    }
 }
 
-export const fb_userLogin = async (email, password, location=null) => {
+export const fb_userLogin = async (email, password, location = null) => {
+   console.log("logging in with the following:", { email, password });
    try {
       const login_credential = await signInWithEmailAndPassword(auth, email, password);
       const currentUser = login_credential.user;
@@ -19,20 +21,19 @@ export const fb_userLogin = async (email, password, location=null) => {
 
       if (!currentUser) throw new Error(`ERROR in fb_userLogin (authorization.jsx), located in ${location}!!!\ncurrentUser = ${currentUser}!!!`)
       
-      return currentUser;
+      return login_credential
    } catch (error) {
       console.error({ message: "userLogin error (authorization.jsx)", location, error, errorMessage: error.message, errorName: error.name });
    }
 }
 
-export const fb_signOut = async () => {
+export const fb_signOut = async (location=null) => {
    try {
       const sign_out = await signOut(auth);
-      console.log({ message: "fb_signOut SUCCESSFUL!!!", authCurrentUser: auth.currentUser, sign_out });
-      
-      return { message: "fb_signOut SUCCESSFUL!!!", authCurrentUser: auth.currentUser, sign_out };
+
+      return { message: "Successfully signed out of Firebase Auth.", sign_out_credential: sign_out };
    } catch (error) {
-      console.error({message: "ERROR in fb_signOut.", error, errorMessage: error.message, errorName: error.name})
+      console.error({message: "ERROR in fb_signOut.", location, error, errorMessage: error.message, errorName: error.name})
    }
 }
 
