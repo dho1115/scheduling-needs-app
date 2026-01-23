7a4d85e13b9527c458fc5e1d143a80e2ff4bf057
admin_dho <davidhorph@hotmail.com>
2026-01-23 13:21:58 -0600

    func. to setCurrentUser state based on auth.
    Created new function, setCurrentUserState which (hopefully) contains the same logic that is found inside THIS getDocs (located inside the useEffect of custom_hooks.jsx):
    
    getDocs(collection(db, "Current User"))
             .then(snapshot => {
                let snapshot_result = {};
                snapshot.docs.forEach(async document => {
                   const snapshot_doc = { id: document.id, docID: document.data().id, docName: document.data() };
    
                   if (auth?.currentUser?.uid) {
                      setCurrentUser({ ...document.data() });
    
                      snapshot_result = { ...snapshot_result, ...{ authCurrentUser: auth?.currentUser?.uid, currentUser } };
                   } else {
                      const deleteDoc = await fb_deleteOneDocument("Current User", snapshot_doc.id)
    
                      setCurrentUser({ id: '', name: '', password: '', role: '' })
    
                      snapshot_result = { ...snapshot_result, ...{ authCurrentUser: auth?.currentUser?.uid, currentUser, deleteDocData: deleteDoc } };
                   }
                });
    
                return snapshot_result
             })
             .then(snapshot_result => console.log({ ...snapshot_result }))
             .catch(error => console.error(error))
    
    This is the logic inside setCurrentUserState:
    
    export const setCurrentUserState = async(currentUser, setCurrentUser_fn_declaration, location=null) => {
       let snapshot_result = {};
       try {
          const currentUserSnapshot = await getDocs(collection(db, "Current User"))
    
          currentUserSnapshot.docs.forEach(async document => {
             const snapshot_doc = { id: document.id, docID: document.data().id, data: document.data() };
    
             if (auth?.currentUser?.uid) {
                setCurrentUser_fn_declaration({ ...document.data }); //setCurrentUser({...document.data});
                snapshot_result = { ...snapshot_result, authCurrentUser: auth.currentUser, uid: auth.currentUser.uid, currentUser };
             } else {
                const deleteCurrentUser = await fb_deleteOneDocument("Current User", currentUser.id);
    
                snapshot_result = { ...snapshot_result, authCurrentUser: auth.currentUser, currentUser, deleteCurrentUser_msg: deleteCurrentUser };
             }
          })
    
          return snapshot_result;
       } catch (error) {
          console.error({ message: "ERROR in setCurrentUserState (miscellaneious.jsx)!!!", location, error, errorName: error.name, errorMessage: error.message });
       }
    }
    
    I plan to replace the long code in getDocs with a setCurrentUserState function call.

--- src/functions/firebase/miscellaneous.jsx
+++ src/functions/firebase/miscellaneous.jsx
@@ -1,5 +1,31 @@
+import { collection, getDoc, getDocs } from "firebase/firestore";
 import { fb_signOut, fb_signUpNewUser, fb_userLogin } from "./authorization";
 import { fb_addOneDocument, fb_deleteOneDocument } from "./crud_basic";
+import { auth, db } from "../../firebase";
+
+export const setCurrentUserState = async(currentUser, setCurrentUser_fn_declaration, location=null) => {
+   let snapshot_result = {};
+   try {
+      const currentUserSnapshot = await getDocs(collection(db, "Current User"))
+
+      currentUserSnapshot.docs.forEach(async document => {
+         const snapshot_doc = { id: document.id, docID: document.data().id, data: document.data() };
+
+         if (auth?.currentUser?.uid) {
+            setCurrentUser_fn_declaration({ ...document.data }); //setCurrentUser({...document.data});
+            snapshot_result = { ...snapshot_result, authCurrentUser: auth.currentUser, uid: auth.currentUser.uid, currentUser };
+         } else {
+            const deleteCurrentUser = await fb_deleteOneDocument("Current User", currentUser.id);
+
+            snapshot_result = { ...snapshot_result, authCurrentUser: auth.currentUser, currentUser, deleteCurrentUser_msg: deleteCurrentUser };
+         }
+      })
+
+      return snapshot_result;
+   } catch (error) {
+      console.error({ message: "ERROR in setCurrentUserState (miscellaneious.jsx)!!!", location, error, errorName: error.name, errorMessage: error.message });
+   }
+}
 
 export const fb_fs_NewUserRegistration = async (signUp_details, location=null) => {
    const fake_email = `${signUp_details.name.split(" ").join("") + signUp_details.id}@email.com`
@@ -28,6 +54,10 @@ export const fb_fs_ExistingUserLogin = async (signUp_details, location) => {
    }
 }
 
+export const fb_fs_get_docID = async (collection) => {
+   return await getDoc(collection)
+}
+
 export const fb_fs_SignOutProcess = async (_currentUserID, setCurrentUserDeclaration, currentUser, location) => {
    try {
       const set_current_user = () => new Promise((res, rej) => {
@@ -40,8 +70,8 @@ export const fb_fs_SignOutProcess = async (_currentUserID, setCurrentUserDeclara
       return await Promise.all([
          fb_signOut(_currentUserID, location),
          fb_deleteOneDocument("Current User", _currentUserID), set_current_user()
-            .then(result => console.log(result))
-            .catch(error => console.error(error))
+            .then(result => console.log("result after set_current_user:", result))
+            .catch(error => console.error({ message: `ERROR logging off ${JSON.stringify(currentUser)} (miscellaneous.jsx)!!!`, location, error, errorMessage: error.message, errorName: error.name }))
       ])
    } catch (error) {
       console.error({ message: "Error in fb_fs_SignOutProcess (miscellaneous.jsx)", location, error, errorMessage: error.message, errorStack: error.stack, errorName: error.name });
