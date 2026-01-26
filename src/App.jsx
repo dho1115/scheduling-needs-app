a5bbb10119f740e96c65a152e4468ef24e819630
admin_dho <davidhorph@hotmail.com>
2026-01-26 16:00:21 -0600

    Update App.jsx
    1. Removed "Current User" from useFetchFirestoreAndSetState b/c it kept giving me an error inside useFetchFirestoreAndSetState of customHooks (MapDataBaseToState[collection_name] not a function error - TypeError).
    
    2. Commented out the <button></button> did not need.
    
    3. Removed any JSON server imports.

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,8 @@ import UpcomingShifts from "./components/private/candidate/shifts_i_confirmed/Up
 import { BatchDelete } from "./functions/deleteRequest";
 import { ConfirmApprovedShiftLogic } from "./functions/emailFunctions";
 import { DateTime } from "luxon";
-import { PatchRequest } from "./functions/patchRequest";
-import { FetchDataSetState } from "./functions/FetchHook";
 import emailjs from '@emailjs/browser';
+import { fb_deleteOneDocument } from "./functions/firebase/crud_basic";
 
 //CustomHooks
 import { useFetchFirestoreAndSetState, useSignUp } from "./functions/custom_hooks";
@@ -29,7 +28,7 @@ import ShiftCandidatesPage from "./pages/supervisor/shift/ShiftCandidatesPage";
 export const ShiftContext = createContext();
 
 function App() {
-  const [currentUser, setCurrentUser, shiftStatuses, setShiftStatuses, employees, setEmployees] = useFetchFirestoreAndSetState(["Current User", "Employees", "Shifts Available", "Shifts With Applicants", "Shifts Pending Confirmation", "Shifts Confirmed"], "App.jsx");
+  const [currentUser, setCurrentUser, shiftStatuses, setShiftStatuses, employees, setEmployees] = useFetchFirestoreAndSetState(["Employees", "Shifts Available", "Shifts With Applicants", "Shifts Pending Confirmation", "Shifts Confirmed"], "App.jsx");
 
   useEffect(() => {
     emailjs.init({ publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY });
@@ -45,32 +44,30 @@ function App() {
     <ShiftContext.Provider
       value={{ customHooks: { useSignUp }, currentUser, setCurrentUser, employees, setEmployees, shiftStatuses, setShiftStatuses, emailjs_keys: { SERVICE_ID, PUBLIC_KEY_ID, GENERAL_KEY_ID, CONFIRM_SHIFT_KEY_ID } }}
     >
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/available shifts" element={<SchedulingNeeds />} />
-          {
-            (currentUser.id && currentUser.name)
-            &&
-            <>
-              <Route path="/supervisor/welcome/:id/*" element={<SupervisorPage />}>
-                <Route path="add shift" element={<AddShift />} />
-                <Route path="available shifts" element={<SchedulingNeeds />} />
-                <Route path="shifts/unconfirmed-shifts" element={<UnconfirmedShifts />} />
-                <Route path="shifts/shift/:_shiftID/candidates" element={<ShiftCandidatesPage />} />
-              </Route>
-              
-              <Route path="/candidate/welcome/:id/*" element={<CandidatePage />}>
-                <Route path="available shifts" element={<SchedulingNeeds />} />
-                <Route path="shifts/applied" element={<ShiftsAppliedFor />} />
-                <Route path="shifts/pending confirmation" element={<ShiftsNeedingConfirmation />} />
-                <Route path="shifts/assigned to work" element={<UpcomingShifts />} />
-              </Route>
-            </>
-          }
-          <Route path="*" element={<Navigate replace to='/' />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/available shifts" element={<SchedulingNeeds />} />
+        {
+          (currentUser.id && currentUser.name)
+          &&
+          <>
+            <Route path="/supervisor/welcome/:id/*" element={<SupervisorPage />}>
+              <Route path="add shift" element={<AddShift />} />
+              <Route path="available shifts" element={<SchedulingNeeds />} />
+              <Route path="shifts/unconfirmed-shifts" element={<UnconfirmedShifts />} />
+              <Route path="shifts/shift/:_shiftID/candidates" element={<ShiftCandidatesPage />} />
+            </Route>
+            
+            <Route path="/candidate/welcome/:id/*" element={<CandidatePage />}>
+              <Route path="available shifts" element={<SchedulingNeeds />} />
+              <Route path="shifts/applied" element={<ShiftsAppliedFor />} />
+              <Route path="shifts/pending confirmation" element={<ShiftsNeedingConfirmation />} />
+              <Route path="shifts/assigned to work" element={<UpcomingShifts />} />
+            </Route>
+          </>
+        }
+        <Route path="*" element={<Navigate replace to='/' />} />
+      </Routes>
     </ShiftContext.Provider>
   )
 }
